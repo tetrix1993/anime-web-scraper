6d87dd62b6cc7c16eda87bb58b4de4114a28c485
tetrix1993 <tetrix1993@hotmail.com>
2020-05-07 19:48:12 +0800

    Improved migration script

--- migrate.py
+++ migrate.py
@@ -1,26 +1,39 @@
 import os
 import shutil
+import traceback
 
 VERSION_NUMBER = 1
 DOWNLOAD_DIR = 'download'
 UNCONFIRMED_DIR = DOWNLOAD_DIR + '/unconfirmed'
+MIGRATION_ERROR_LOG = 'migration_error.log'
 
 VERSION_FILE = 'migrate_version'
 MOVING_IMAGE_TEMPLATE = 'Moved folder %s to %s'
 
+is_successful = True
+
+
+def migrate_folders():
+    migrate_folder('2020-2/oregairu3', UNCONFIRMED_DIR + '/oregairu3')
+    migrate_folder('2020-3/maohgakuin', UNCONFIRMED_DIR + '/maohgakuin')
+    migrate_folder('2020-3/rezero2', UNCONFIRMED_DIR + '/rezero2')
+
 
 def run():
-    if is_latest_version():
-        return
     print('Running migration script version ' + str(VERSION_NUMBER))
-    if not os.path.exists('download/unconfirmed'):
-        os.makedirs(UNCONFIRMED_DIR)
-    migrate_oregairu3()
-    migrate_maohgakuin()
-    migrate_rezero2()
-    delete_empty_folders()
-    update_version()
-    print('Migration completed.')
+    try:
+        if is_latest_version():
+            return
+        if not os.path.exists('download/unconfirmed'):
+            os.makedirs(UNCONFIRMED_DIR)
+        migrate_folders()
+        delete_empty_folders()
+        update_version()
+        print('Migration completed.')
+    except Exception as e:
+        with open(MIGRATION_ERROR_LOG, 'a+') as f:
+            f.write(traceback.format_exc())
+        print('Migration failed. See error log - ' + MIGRATION_ERROR_LOG)
 
 
 def is_latest_version():
@@ -36,37 +49,25 @@ def is_latest_version():
 
 
 def update_version():
-    with open(VERSION_FILE, 'w+') as f:
-        f.write(str(VERSION_NUMBER))
+    if is_successful:
+        with open(VERSION_FILE, 'w+') as f:
+            f.write(str(VERSION_NUMBER))
 
 
-def print_moving_message(old_dir, new_dir):
-    message = MOVING_IMAGE_TEMPLATE % (old_dir, new_dir)
-    print(message)
-
-
-def migrate_oregairu3():
-    old_dir = 'download/2020-2/oregairu3'
+def migrate_folder(old_dir, new_dir):
+    global is_successful
+    if len(old_dir) > 9 and old_dir[0:9] != "download/":
+        old_dir = DOWNLOAD_DIR + '/' + old_dir
+    if len(new_dir) > 9 and new_dir[0:9] != "download/":
+        new_dir = DOWNLOAD_DIR + '/' + new_dir
     if os.path.exists(old_dir):
-        new_dir = UNCONFIRMED_DIR + '/oregairu3'
+        if os.path.exists(new_dir):
+            print('Failed to migrate %s to %s - Folder to be migrated exists' % (old_dir, new_dir))
+            is_successful = False
+            return
         shutil.move(old_dir, new_dir)
-        print_moving_message(old_dir, new_dir)
-
-
-def migrate_maohgakuin():
-    old_dir = 'download/2020-3/maohgakuin'
-    if os.path.exists(old_dir):
-        new_dir = UNCONFIRMED_DIR + '/maohgakuin'
-        shutil.move(old_dir, new_dir)
-        print_moving_message(old_dir, new_dir)
-
-
-def migrate_rezero2():
-    old_dir = 'download/2020-3/rezero2'
-    if os.path.exists(old_dir):
-        new_dir = UNCONFIRMED_DIR + '/rezero2'
-        shutil.move(old_dir, new_dir)
-        print_moving_message(old_dir, new_dir)
+        message = MOVING_IMAGE_TEMPLATE % (old_dir, new_dir)
+        print(message)
 
 
 def delete_empty_folders():
