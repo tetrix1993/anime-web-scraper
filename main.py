330261447fdf6970a0606696ed4d4043f3d5258c
tetrix1993 <tetrix1993@hotmail.com>
2020-11-27 23:18:06 +0800

    Removed MonIsha and Uzakichan from main.py

--- main.py
+++ main.py
@@ -12,8 +12,7 @@ def run():
     if not os.path.exists(PROCESS_FOLDER):
         os.makedirs(PROCESS_FOLDER)
 
-    downloads = [HxErosDownload(), KanokariDownload(), MaohgakuinDownload(), MonIshaDownload(),
-                  ReZero2Download(), UzakiChanDownload()]
+    downloads = [HxErosDownload(), KanokariDownload(), MaohgakuinDownload(), ReZero2Download()]
     subclasses = Fall2020AnimeDownload.__subclasses__() \
         + Winter2021AnimeDownload.__subclasses__() \
         + Spring2021AnimeDownload.__subclasses__() \
