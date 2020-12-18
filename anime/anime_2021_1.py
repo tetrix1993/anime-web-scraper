44a6fcf9a8dcd25427989ef8a9f221c9863db1ef
tetrix1993 <tetrix1993@hotmail.com>
2020-12-19 01:05:33 +0800

    Updated TomozakiKun Bluray

--- anime/anime_2021_1.py
+++ anime/anime_2021_1.py
@@ -241,6 +241,10 @@ class TomozakiKunDownload(Winter2021AnimeDownload):
 
     def download_bluray(self):
         folder = self.create_bluray_directory()
+        self.image_list = []
+        self.add_to_image_list('bd_tw_1_1', 'https://pbs.twimg.com/media/EphcE8pUUAASocS?format=jpg&name=900x900')
+        self.add_to_image_list('bd_tw_1_2', 'https://pbs.twimg.com/media/EphcE8nVEAEVsec?format=jpg&name=900x900')
+        self.download_image_list(folder)
         self.download_bluray_volume(folder)
         self.download_bluray_bonus(folder)
 
