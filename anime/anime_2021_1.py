172e3759d0fb39601e2698f817589132270e320c
tetrix1993 <tetrix1993@hotmail.com>
2021-01-09 00:37:41 +0800

    Added Lasdan Guess

--- anime/anime_2021_1.py
+++ anime/anime_2021_1.py
@@ -1044,12 +1044,15 @@ class LasdanDownload(Winter2021AnimeDownload):
     folder_name = 'lasdan'
 
     PAGE_PREFIX = 'https://lasdan.com/'
+    FINAL_EPISODE = 12
+    IMAGES_PER_EPISODE = 6
 
     def __init__(self):
         super().__init__()
 
     def run(self):
         self.download_episode_preview()
+        self.download_episode_preview_guess()
         self.download_key_visual()
         self.download_character()
         self.download_bluray()
@@ -1082,6 +1085,27 @@ class LasdanDownload(Winter2021AnimeDownload):
         except Exception as e:
             print("Error in running " + self.__class__.__name__)
             print(e)
+
+    def download_episode_preview_guess(self):
+        folder = self.create_custom_directory('guess')
+        template = self.PAGE_PREFIX + 'core_sys/images/contents/%s/block/%s/%s.jpg'
+        for i in range(self.FINAL_EPISODE):
+            episode = str(i + 1).zfill(2)
+            if self.is_image_exists(episode + '_1'):
+                continue
+            first = 22 + i
+            second = 35 + 3 * i
+            third = 49 + self.IMAGES_PER_EPISODE * i
+            for j in range(self.IMAGES_PER_EPISODE):
+                image_url = template % (str(first).zfill(8), str(second).zfill(8), str(third + j).zfill(8))
+                image_name = episode + '_' + str(j + 1)
+                result = self.download_image(image_url, folder + '/' + image_name)
+                if result == -1:
+                    if len(os.listdir(folder)) == 0:
+                        os.rmdir(folder)
+                    return
+        if len(os.listdir(folder)) == 0:
+            os.rmdir(folder)
 
     def download_key_visual(self):
         filepath = self.create_key_visual_directory()
