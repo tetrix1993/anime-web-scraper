c75b7b6b1a634b7396e1ab4651d41dbf37262ce4
tetrix1993 <tetrix1993@hotmail.com>
2021-03-19 23:06:01 +0800

    Added EightySix News

--- anime/anime_2021_2.py
+++ anime/anime_2021_2.py
@@ -48,12 +48,55 @@ class EightySixDownload(Spring2021AnimeDownload):
 
     def run(self):
         self.download_episode_preview()
+        self.download_news()
         self.download_key_visual()
         self.download_character()
         self.download_media()
 
     def download_episode_preview(self):
         self.has_website_updated(self.PAGE_PREFIX, 'index', diff=2)
+
+    def download_news(self):
+        news_url = self.PAGE_PREFIX + 'news/'
+        stop = False
+        try:
+            results = []
+            news_obj = self.get_last_news_log_object()
+            for page in range(1, 100, 1):
+                page_url = news_url
+                if page > 1:
+                    page_url = news_url + '?p=' + str(page)
+                soup = self.get_soup(page_url, decode=True)
+                lis = soup.find_all('li', class_='c-list__item')
+                for li in lis:
+                    tag_date = li.find('div', class_='c-list__item-date')
+                    tag_title = li.find('div', class_='c-list__item-title')
+                    a_tag = li.find('a')
+                    if tag_date and tag_title and a_tag and a_tag.has_attr('href'):
+                        article_id = news_url + a_tag['href'].replace('./', '')
+                        date = tag_date.text.strip()
+                        title = tag_title.text.strip()
+                        if news_obj and news_obj['id'] == article_id:
+                            stop = True
+                            break
+                        results.append(self.create_news_log_object(date, title, article_id))
+                if stop:
+                    break
+                next_page_tag = soup.select('div.c-pagination__arrow.-next')
+                if len(next_page_tag) == 0:
+                    break
+                if next_page_tag[0].has_attr('class') and 'is-disable' in next_page_tag[0]['class']:
+                    break
+            success_count = 0
+            for result in reversed(results):
+                process_result = self.create_news_log_from_news_log_object(result)
+                if process_result == 0:
+                    success_count += 1
+            if len(results) > 0:
+                self.create_news_log_cache(success_count, results[0])
+        except Exception as e:
+            print("Error in running " + self.__class__.__name__ + ' - News')
+            print(e)
 
     def download_key_visual(self):
         folder = self.create_key_visual_directory()
