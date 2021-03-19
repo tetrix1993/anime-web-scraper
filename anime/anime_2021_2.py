dd8d872f6ed0714227d5a52226b298ceaff19ccb
tetrix1993 <tetrix1993@hotmail.com>
2021-03-19 23:55:50 +0800

    Added Higehiro News

--- anime/anime_2021_2.py
+++ anime/anime_2021_2.py
@@ -354,11 +354,47 @@ class HigehiroDownload(Spring2021AnimeDownload):
 
     def run(self):
         self.download_episode_preview()
+        self.download_news()
         self.download_key_visual()
         self.download_character()
 
     def download_episode_preview(self):
         self.has_website_updated(self.PAGE_PREFIX, 'index', diff=2)
+
+    def download_news(self):
+        news_url = self.PAGE_PREFIX + 'news/'
+        try:
+            soup = self.get_soup(news_url, decode=True)
+            ul = soup.find('ul', class_='news')
+            if not ul:
+                return
+            lis = ul.find_all('li')
+            news_obj = self.get_last_news_log_object()
+            results = []
+            for li in lis:
+                if not li.has_attr('id'):
+                    continue
+                tag_date = li.find('span')
+                tag_title = li.find('h3')
+                if tag_date and tag_title:
+                    article_id = li['id']
+                    date = self.format_news_date(tag_date.text)
+                    if len(date) == 0:
+                        continue
+                    title = tag_title.text.split(tag_date.text)[1].strip()
+                    if news_obj and news_obj['id'] == article_id:
+                        break
+                    results.append(self.create_news_log_object(date, title, article_id))
+            success_count = 0
+            for result in reversed(results):
+                process_result = self.create_news_log_from_news_log_object(result)
+                if process_result == 0:
+                    success_count += 1
+            if len(results) > 0:
+                self.create_news_log_cache(success_count, results[0])
+        except Exception as e:
+            print("Error in running " + self.__class__.__name__ + ' - News')
+            print(e)
 
     def download_key_visual(self):
         folder = self.create_key_visual_directory()
