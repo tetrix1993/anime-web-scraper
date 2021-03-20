e79caa0b4004dddb1d54f038d44a0b9dcff977db
tetrix1993 <tetrix1993@hotmail.com>
2021-03-20 15:14:11 +0800

    Added KobayashiMaidDragon2 News

--- anime/anime_2021_3.py
+++ anime/anime_2021_3.py
@@ -115,7 +115,6 @@ class BokuremaDownload(Summer2021AnimeDownload):
         self.download_image_list(folder)
 
 
-
 # Cheat Kusushi no Slow Life: Isekai ni Tsukurou Drugstore
 class CheatKusushiDownload(Summer2021AnimeDownload):
     title = 'Cheat Kusushi no Slow Life: Isekai ni Tsukurou Drugstore'
@@ -242,10 +241,54 @@ class KobayashiMaidDragon2Download(Summer2021AnimeDownload):
 
     def run(self):
         self.download_episode_preview()
+        self.download_news()
         self.download_key_visual()
 
     def download_episode_preview(self):
         self.has_website_updated(self.PAGE_PREFIX, 'index', diff=2)
+
+    def download_news(self):
+        prefix = 'https://maidragon.jp'
+        news_url = prefix + '/news/'
+        stop = False
+        try:
+            results = []
+            news_obj = self.get_last_news_log_object()
+            for page in range(1, 100, 1):
+                page_url = news_url
+                if page > 1:
+                    page_url = news_url + 'page/' + str(page)
+                soup = self.get_soup(page_url, decode=True)
+                articles = soup.select('article.c-news-item')
+                for article in articles:
+                    tag_date = article.find('span', class_='c-news-item__date')
+                    tag_title = article.find('span', class_='c-news-item__title')
+                    a_tag = article.find('a')
+                    if tag_date and tag_title and a_tag and a_tag.has_attr('href'):
+                        article_id = prefix + a_tag['href']
+                        date = self.format_news_date(tag_date.text.strip())
+                        if len(date) == 0:
+                            continue
+                        title = tag_title.text.strip()
+                        if date.startswith('2019') or (news_obj and news_obj['id'] == article_id):
+                            stop = True
+                            break
+                        results.append(self.create_news_log_object(date, title, article_id))
+                if stop:
+                    break
+                pagination_lis = soup.select('ul.page-numbers li a.next.page-numbers')
+                if len(pagination_lis) == 0:
+                    break
+            success_count = 0
+            for result in reversed(results):
+                process_result = self.create_news_log_from_news_log_object(result)
+                if process_result == 0:
+                    success_count += 1
+            if len(results) > 0:
+                self.create_news_log_cache(success_count, results[0])
+        except Exception as e:
+            print("Error in running " + self.__class__.__name__ + ' - News')
+            print(e)
 
     def download_key_visual(self):
         folder = self.create_key_visual_directory()
