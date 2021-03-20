6a998771a118a084d48743851d114852c1dbb0a7
tetrix1993 <tetrix1993@hotmail.com>
2021-03-20 16:02:56 +0800

    Added ShinnoNakama News

--- anime/anime_2021_3.py
+++ anime/anime_2021_3.py
@@ -578,10 +578,40 @@ class ShinnoNakamaDownload(Summer2021AnimeDownload):
 
     def run(self):
         self.download_episode_preview()
+        self.download_news()
         self.download_key_visual()
 
     def download_episode_preview(self):
         self.has_website_updated(self.PAGE_PREFIX, 'index')
+
+    def download_news(self):
+        news_url = self.PAGE_PREFIX + 'news/'
+        try:
+            soup = self.get_soup(news_url, decode=True)
+            lis = soup.select('ul.newsListsWrap li')
+            news_obj = self.get_last_news_log_object()
+            results = []
+            for li in lis:
+                tag_date = li.find('p', class_='update_time')
+                tag_title = li.find('p', class_='update_ttl')
+                a_tag = li.find('a')
+                if tag_date and tag_title and a_tag and a_tag.has_attr('href'):
+                    article_id = news_url + a_tag['href'].replace('./', '')
+                    date = tag_date.text.strip()
+                    title = tag_title.text.strip()
+                    if news_obj and news_obj['id'] == article_id:
+                        break
+                    results.append(self.create_news_log_object(date, title, article_id))
+            success_count = 0
+            for result in reversed(results):
+                process_result = self.create_news_log_from_news_log_object(result)
+                if process_result == 0:
+                    success_count += 1
+            if len(results) > 0:
+                self.create_news_log_cache(success_count, results[0])
+        except Exception as e:
+            print("Error in running " + self.__class__.__name__ + ' - News')
+            print(e)
 
     def download_key_visual(self):
         folder = self.create_key_visual_directory()
