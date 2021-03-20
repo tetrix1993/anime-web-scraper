19790a72bc97c5fc1cc23b1d589b66b6112aaf65
tetrix1993 <tetrix1993@hotmail.com>
2021-03-20 16:10:28 +0800

    Added Aquatope News

--- anime/anime_2021_3.py
+++ anime/anime_2021_3.py
@@ -463,7 +463,7 @@ class PeachBoyRiversideDownload(Summer2021AnimeDownload):
                     tag_date = li.find('h3')
                     tag_title = li.find('h4', class_='ttl')
                     a_tag = li.find('a')
-                    if len(tag_date) > 0 and tag_title and a_tag and a_tag.has_attr('href'):
+                    if tag_date and tag_title and a_tag and a_tag.has_attr('href'):
                         article_id = a_tag['href']
                         date = tag_date.text.strip()
                         title = tag_title.text.strip()
@@ -633,11 +633,56 @@ class AquatopeDownload(Summer2021AnimeDownload):
 
     def run(self):
         self.download_episode_preview()
+        self.download_news()
         self.download_key_visual()
         self.download_character()
 
     def download_episode_preview(self):
         self.has_website_updated(self.PAGE_PREFIX, 'index')
+
+    def download_news(self):
+        news_url = self.PAGE_PREFIX + 'news/'
+        stop = False
+        try:
+            results = []
+            news_obj = self.get_last_news_log_object()
+            for page in range(1, 2, 1):
+                page_url = news_url
+                if page > 1:
+                    page_url = news_url + 'page/' + str(page) + '/'
+                soup = self.get_soup(page_url, decode=True)
+                articles = soup.select('article.news--lineup--block')
+                for article in articles:
+                    tag_date = article.find('dt')
+                    tag_title = article.find('h3')
+                    a_tag = article.find('a')
+                    if tag_date and tag_title and a_tag and a_tag.has_attr('href'):
+                        article_id = a_tag['href']
+                        date = self.format_news_date(tag_date.text.strip())
+                        if len(date) == 0:
+                            continue
+                        title = tag_title.text.strip()
+                        if news_obj and news_obj['id'] == article_id:
+                            stop = True
+                            break
+                        results.append(self.create_news_log_object(date, title, article_id))
+                if stop:
+                    break
+                pagination = soup.select('ul.pagenation-list li')
+                if len(pagination) == 0:
+                    break
+                if pagination[-1].has_attr('class') and 'is__current' in pagination[-1]['class']:
+                    break
+            success_count = 0
+            for result in reversed(results):
+                process_result = self.create_news_log_from_news_log_object(result)
+                if process_result == 0:
+                    success_count += 1
+            if len(results) > 0:
+                self.create_news_log_cache(success_count, results[0])
+        except Exception as e:
+            print("Error in running " + self.__class__.__name__ + ' - News')
+            print(e)
 
     def download_key_visual(self):
         folder = self.create_key_visual_directory()
