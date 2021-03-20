10059f7fbda81efafc6ad946bd7d7d377c1499bb
tetrix1993 <tetrix1993@hotmail.com>
2021-03-20 20:25:35 +0800

    Added TsukiLaikaNosferatu News

--- anime/unconfirmed.py
+++ anime/unconfirmed.py
@@ -838,10 +838,55 @@ class TsukiLaikaNosferatuDownload(UnconfirmedDownload):
 
     def run(self):
         self.download_episode_preview()
+        self.download_news()
         self.download_key_visual()
 
     def download_episode_preview(self):
         self.has_website_updated(self.PAGE_PREFIX, 'index')
+
+    def download_news(self):
+        news_url = self.PAGE_PREFIX + 'news/'
+        stop = False
+        try:
+            results = []
+            news_obj = self.get_last_news_log_object()
+            for page in range(1, 100, 1):
+                page_url = news_url
+                if page > 1:
+                    page_url = news_url + 'page/' + str(page) + '/'
+                soup = self.get_soup(page_url, decode=True)
+                articles = soup.find_all('article', class_='news-box')
+                for article in articles:
+                    tag_date = article.find('p', 'news-box-date')
+                    tag_title = article.find('h3', class_='news-box-ttl')
+                    a_tag = article.find('a')
+                    if tag_date and tag_title and a_tag and a_tag.has_attr('href'):
+                        article_id = a_tag['href']
+                        date = self.format_news_date(tag_date.text.strip())
+                        if len(date) == 0:
+                            continue
+                        title = tag_title.text.strip()
+                        if news_obj and news_obj['id'] == article_id:
+                            stop = True
+                            break
+                        results.append(self.create_news_log_object(date, title, article_id))
+                if stop:
+                    break
+                pagination = soup.select('ul.pagenation-list li')
+                if len(pagination) == 0:
+                    break
+                if pagination[-1].has_attr('class') and 'is__current' in pagination[-1]['class']:
+                    break
+            success_count = 0
+            for result in reversed(results):
+                process_result = self.create_news_log_from_news_log_object(result)
+                if process_result == 0:
+                    success_count += 1
+            if len(results) > 0:
+                self.create_news_log_cache(success_count, results[0])
+        except Exception as e:
+            print("Error in running " + self.__class__.__name__ + ' - News')
+            print(e)
 
     def download_key_visual(self):
         folder = self.create_key_visual_directory()
