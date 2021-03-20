9b8e719b6ddfa4b8ef126b92f3fb171dc888de5d
tetrix1993 <tetrix1993@hotmail.com>
2021-03-20 19:24:58 +0800

    Added Kanokano News

--- anime/unconfirmed.py
+++ anime/unconfirmed.py
@@ -151,10 +151,41 @@ class KanokanoDownload(UnconfirmedDownload):
 
     def run(self):
         self.download_episode_preview()
+        self.download_news()
         self.download_key_visual()
 
     def download_episode_preview(self):
         self.has_website_updated(self.PAGE_PREFIX, 'index')
+
+    def download_news(self):
+        news_url = self.PAGE_PREFIX
+        try:
+            soup = self.get_soup(news_url, decode=True)
+            articles = soup.select('ul.news-list li.news-list-node')
+            news_obj = self.get_last_news_log_object()
+            results = []
+            for article in articles:
+                tag_date = article.find('div', class_='news-date')
+                tag_title = article.find('div', class_='news-title')
+                if tag_date and tag_title:
+                    article_id = ''
+                    date = self.format_news_date(tag_date.text.strip().replace('/', '.'))
+                    if len(date) == 0:
+                        continue
+                    title = tag_title.text.strip()
+                    if news_obj and news_obj['date'] == date and news_obj['title'] == title:
+                        break
+                    results.append(self.create_news_log_object(date, title, article_id))
+            success_count = 0
+            for result in reversed(results):
+                process_result = self.create_news_log_from_news_log_object(result)
+                if process_result == 0:
+                    success_count += 1
+            if len(results) > 0:
+                self.create_news_log_cache(success_count, results[0])
+        except Exception as e:
+            print("Error in running " + self.__class__.__name__ + ' - News')
+            print(e)
 
     def download_key_visual(self):
         folder = self.create_key_visual_directory()
