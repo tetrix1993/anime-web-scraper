bdd7967185d68907368cdf6da223d97c1a611163
tetrix1993 <tetrix1993@hotmail.com>
2021-03-20 13:13:07 +0800

    Added SayonaraCramer News

--- anime/anime_2021_2.py
+++ anime/anime_2021_2.py
@@ -810,11 +810,54 @@ class SayonaraCramerDownload(Spring2021AnimeDownload):
 
     def run(self):
         self.download_episode_preview()
+        self.download_news()
         self.download_key_visual()
         self.download_character()
 
     def download_episode_preview(self):
         self.has_website_updated(self.PAGE_PREFIX + 'tv/', 'index')
+
+    def download_news(self):
+        news_url = self.PAGE_PREFIX + 'news/'
+        stop = False
+        try:
+            results = []
+            news_obj = self.get_last_news_log_object()
+            for page in range(1, 100, 1):
+                page_url = news_url
+                if page > 1:
+                    page_url = news_url + 'page/' + str(page) + '/'
+                soup = self.get_soup(page_url, decode=True)
+                articles = soup.find_all('article', class_='news-main__block')
+                for article in articles:
+                    tag_date = article.select('div.news-main__block--date span')
+                    tag_title = article.find('div', class_='ttl')
+                    a_tag = article.find('a')
+                    if len(tag_date) > 0 and tag_title and a_tag and a_tag.has_attr('href'):
+                        article_id = a_tag['href']
+                        date = tag_date[0].text.strip()
+                        title = tag_title.text.strip()
+                        if news_obj and news_obj['id'] == article_id:
+                            stop = True
+                            break
+                        results.append(self.create_news_log_object(date, title, article_id))
+                if stop:
+                    break
+                pagination = soup.select('ul.pagenation-list li')
+                if len(pagination) == 0:
+                    break
+                if pagination[-1].has_attr('class') and 'is__current' in pagination[-1]['class']:
+                    break
+            success_count = 0
+            for result in reversed(results):
+                process_result = self.create_news_log_from_news_log_object(result)
+                if process_result == 0:
+                    success_count += 1
+            if len(results) > 0:
+                self.create_news_log_cache(success_count, results[0])
+        except Exception as e:
+            print("Error in running " + self.__class__.__name__ + ' - News')
+            print(e)
 
     def download_key_visual(self):
         folder = self.create_key_visual_directory()
