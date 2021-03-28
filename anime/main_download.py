337d4d92e8d19985dce60fa85d4588ba7227a829
tetrix1993 <tetrix1993@hotmail.com>
2021-03-28 14:56:18 +0800

    Updated download_by_template method

--- anime/main_download.py
+++ anime/main_download.py
@@ -787,10 +787,12 @@ class MainDownload:
         while i <= end:
             image_url = template % str(i).zfill(zfill)
             image_name = self.extract_image_name_from_url(image_url, with_extension=False)
+            i += 1
+            if self.is_image_exists(image_name, folder):
+                continue
             result = self.download_image(image_url, folder + '/' + image_name, headers, to_jpg, is_mocanews, min_width)
             if result == -1:
                 break
-            i += 1
 
     # Match filter
     def match(self, s_filter):
