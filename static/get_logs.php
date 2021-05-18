687e7e1e54de6485335c346e7b7f2d7b47bfc79f
tetrix1993 <tetrix1993@hotmail.com>
2021-05-19 00:36:28 +0800

    Content Viewer - Update log display

--- static/get_logs.php
+++ static/get_logs.php
@@ -5,23 +5,22 @@
     <title>Content Viewer Logs</title>
 </head>
 <body style="background-color: #dddddd;">
+<pre style="word-wrap: break-word; line-height: 1.1em;">
 <?php
     if (isset($_GET["name"])) {
         $filepath = '../out/'.$_GET["name"].'_log.tsv';
-        echo '<pre style="word-wrap: break-word; line-height: 0.6em;">';
-        echo @nl2br(@file_get_contents($filepath));
+        //echo @nl2br(@file_get_contents($filepath));
+        echo @htmlspecialchars(@file_get_contents($filepath));
         echo '</pre><script src="global_log.js"></script>';
     }
     elseif (isset($_GET["dir"]) && str_ends_with($_GET["dir"], '.html')) {
         $filepath = $_GET["dir"];
-        echo '<pre style="word-wrap: break-word; line-height: 1em;">';
         echo @htmlspecialchars(@file_get_contents($filepath));
         echo '</pre>';
     }
     elseif (isset($_GET["dir"])) {
         $filepath = $_GET["dir"];
-        echo '<pre style="word-wrap: break-word; line-height: 0.6em;">';
-        echo @nl2br(@file_get_contents($filepath));
+        echo @htmlspecialchars(@file_get_contents($filepath));
         echo '</pre><script src="global_log.js"></script>';
     }
     else {
