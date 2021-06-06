e947eceafc3a7016ce3db2b0a7c75ea4a60538eb
tetrix1993 <tetrix1993@hotmail.com>
2021-06-06 22:20:12 +0800

    Updated HyakumanNoInochi2

--- anime/anime_2021_3.py
+++ anime/anime_2021_3.py
@@ -63,48 +63,6 @@ class HyakumanNoInochi2Download(Summer2021AnimeDownload, NewsTemplate1):
                                    id_select='a', a_tag_prefix=self.PAGE_PREFIX, stop_date='2020',
                                    next_page_select='ul.page-numbers li a.next.page-numbers')
 
-        news_url = self.PAGE_PREFIX + '/news/'
-        stop = False
-        try:
-            results = []
-            news_obj = self.get_last_news_log_object()
-            for page in range(1, 100, 1):
-                page_url = news_url
-                if page > 1:
-                    page_url = news_url + 'page/' + str(page)
-                soup = self.get_soup(page_url, decode=True)
-                articles = soup.select('article.c-news-item')
-                for article in articles:
-                    tag_date = article.find('span', class_='c-news-item__date')
-                    tag_title = article.find('span', class_='c-news-item__title')
-                    a_tag = article.find('a')
-                    if tag_date and tag_title and a_tag and a_tag.has_attr('href'):
-                        article_id = self.PAGE_PREFIX + a_tag['href']
-                        date = self.format_news_date(tag_date.text.strip())
-                        if len(date) == 0:
-                            continue
-                        title = ' '.join(tag_title.text.strip().split())
-                        if date.startswith('2020') or (news_obj and
-                                                       (news_obj['id'] == article_id or date < news_obj['date'])):
-                            stop = True
-                            break
-                        results.append(self.create_news_log_object(date, title, article_id))
-                if stop:
-                    break
-                pagination_lis = soup.select('ul.page-numbers li a.next.page-numbers')
-                if len(pagination_lis) == 0:
-                    break
-            success_count = 0
-            for result in reversed(results):
-                process_result = self.create_news_log_from_news_log_object(result)
-                if process_result == 0:
-                    success_count += 1
-            if len(results) > 0:
-                self.create_news_log_cache(success_count, results[0])
-        except Exception as e:
-            print("Error in running " + self.__class__.__name__ + ' - News')
-            print(e)
-
     def download_key_visual(self):
         folder = self.create_key_visual_directory()
         self.add_to_image_list('teaser', self.PAGE_PREFIX + '/img/home/visual_03.jpg')
