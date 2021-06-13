6433fd5843a189fd5d698680cf4b9d1c39f950b3
tetrix1993 <tetrix1993@hotmail.com>
2021-06-13 15:10:23 +0800

    Updated Dea5 News

--- anime/anime_2021_3.py
+++ anime/anime_2021_3.py
@@ -212,7 +212,7 @@ class CheatKusushiDownload(Summer2021AnimeDownload):
 
 
 # Deatte 5-byou de Battle
-class Dea5Download(Summer2021AnimeDownload):
+class Dea5Download(Summer2021AnimeDownload, NewsTemplate1):
     title = 'Deatte 5-byou de Battle'
     keywords = [title, 'Battle Game in 5 Seconds', 'Dea5']
     website = 'https://dea5-anime.com/'
@@ -234,47 +234,8 @@ class Dea5Download(Summer2021AnimeDownload):
         self.has_website_updated(self.PAGE_PREFIX, 'index')
 
     def download_news(self):
-        news_url = self.PAGE_PREFIX + 'news/'
-        stop = False
-        try:
-            results = []
-            news_obj = self.get_last_news_log_object()
-            for page in range(1, 2, 1):
-                if page == 1:
-                    page_url = news_url
-                else:
-                    page_url = news_url + 'page/' + str(page)
-                soup = self.get_soup(page_url, decode=True)
-                articles = soup.select('div.posts div.post')
-                for article in articles:
-                    tag_date = article.find('p', class_='post_date')
-                    tag_title = article.find('p', class_='post_title')
-                    a_tag = article.find('a')
-                    if tag_date and tag_title:
-                        article_id = ''
-                        if a_tag and a_tag.has_attr('href'):
-                            article_id = self.PAGE_PREFIX + a_tag['href'].replace('../', '')
-                        date = self.format_news_date(tag_date.text)
-                        if len(date) == 0:
-                            continue
-                        title = tag_title.text.strip()
-                        if news_obj and ((news_obj['id'] == article_id and news_obj['title'] == title)
-                                         or date < news_obj['date']):
-                            stop = True
-                            break
-                        results.append(self.create_news_log_object(date, title, article_id))
-                if stop:
-                    break
-            success_count = 0
-            for result in reversed(results):
-                process_result = self.create_news_log_from_news_log_object(result)
-                if process_result == 0:
-                    success_count += 1
-            if len(results) > 0:
-                self.create_news_log_cache(success_count, results[0])
-        except Exception as e:
-            print("Error in running " + self.__class__.__name__ + ' - News')
-            print(e)
+        self.download_template_news(page_prefix=self.PAGE_PREFIX, article_select='div.posts div.post',
+                                    date_select='p.post_date', title_select='p.post_title', id_select='a')
 
     def download_key_visual(self):
         folder = self.create_key_visual_directory()
