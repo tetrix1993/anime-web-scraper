2d4383691b1ff5031ce27d797c444af9f3510e02
tetrix1993 <tetrix1993@hotmail.com>
2021-06-13 22:22:18 +0800

    Updated Hamehura2 News

--- anime/anime_2021_3.py
+++ anime/anime_2021_3.py
@@ -679,7 +679,7 @@ class MeikyuBCDownload(Summer2021AnimeDownload):
 
 
 # Otome Game no Hametsu Flag shika Nai Akuyaku Reijou ni Tensei shiteshimatta... X
-class Hamehura2Download(Summer2021AnimeDownload):
+class Hamehura2Download(Summer2021AnimeDownload, NewsTemplate1):
     title = "Otome Game no Hametsu Flag shika Nai Akuyaku Reijou ni Tensei shiteshimatta... X"
     keywords = [title, "Hamehura", "Hamefura", "My Next Life as a Villainess: All Routes Lead to Doom!", "2nd"]
     website = 'https://hamehura-anime.com/'
@@ -702,46 +702,10 @@ class Hamehura2Download(Summer2021AnimeDownload):
         self.has_website_updated(self.PAGE_PREFIX, 'index')
 
     def download_news(self):
-        news_url = self.PAGE_PREFIX + 'news/'
-        stop = False
-        try:
-            results = []
-            news_obj = self.get_last_news_log_object()
-            for page in range(1, 2, 1):
-                page_url = news_url
-                if page > 1:
-                    page_url = news_url + 'page/' + str(page) + '/'
-                soup = self.get_soup(page_url, decode=True)
-                articles = soup.find_all('article', class_='md-newsblock')
-                for article in articles:
-                    tag_date = article.find('dt')
-                    tag_title = article.find('h3')
-                    a_tag = article.find('a')
-                    if tag_date and tag_title and a_tag and a_tag.has_attr('href'):
-                        article_id = a_tag['href']
-                        date = tag_date.text.strip()
-                        title = tag_title.text.strip()
-                        if news_obj and (news_obj['id'] == article_id or date < news_obj['date']):
-                            stop = True
-                            break
-                        results.append(self.create_news_log_object(date, title, article_id))
-                if stop:
-                    break
-                pagination = soup.select('ul.pagenation-list li')
-                if len(pagination) == 0:
-                    break
-                if pagination[-1].has_attr('class') and 'is__current' in pagination[-1]['class']:
-                    break
-            success_count = 0
-            for result in reversed(results):
-                process_result = self.create_news_log_from_news_log_object(result)
-                if process_result == 0:
-                    success_count += 1
-            if len(results) > 0:
-                self.create_news_log_cache(success_count, results[0])
-        except Exception as e:
-            print("Error in running " + self.__class__.__name__ + ' - News')
-            print(e)
+        self.download_template_news(page_prefix=self.PAGE_PREFIX, article_select='article.md-newsblock',
+                                    date_select='dt', title_select='h3', id_select='a',
+                                    next_page_select='ul.pagenation-list li',
+                                    next_page_eval_index_class='is__current', next_page_eval_index=-1)
 
     def download_key_visual(self):
         folder = self.create_key_visual_directory()
