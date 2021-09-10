5154cb6356b3a1ccbc5ef39cd6d7b0b8b8a7aa22
tetrix1993 <tetrix1993@hotmail.com>
2021-09-10 19:43:02 +0800

    Updated ShinnoNakama News

--- anime/anime_2021_4.py
+++ anime/anime_2021_4.py
@@ -423,7 +423,7 @@ class SenpaigaUzaiDownload(Fall2021AnimeDownload, NewsTemplate):
 
 
 # Shin no Nakama ja Nai to Yuusha no Party wo Oidasareta node, Henkyou de Slow Life suru Koto ni Shimashita
-class ShinnoNakamaDownload(Fall2021AnimeDownload):
+class ShinnoNakamaDownload(Fall2021AnimeDownload, NewsTemplate):
     title = 'Shin no Nakama ja Nai to Yuusha no Party wo Oidasareta node, Henkyou de Slow Life suru Koto ni Shimashita'
     keywords = [title, 'Shinnonakama', "Banished From The Heroes' Party"]
     website = 'https://shinnonakama.com/'
@@ -444,32 +444,9 @@ class ShinnoNakamaDownload(Fall2021AnimeDownload):
 
     def download_news(self):
         news_url = self.PAGE_PREFIX + 'news/'
-        try:
-            soup = self.get_soup(news_url, decode=True)
-            lis = soup.select('ul.newsListsWrap li')
-            news_obj = self.get_last_news_log_object()
-            results = []
-            for li in lis:
-                tag_date = li.find('p', class_='update_time')
-                tag_title = li.find('p', class_='update_ttl')
-                a_tag = li.find('a')
-                if tag_date and tag_title and a_tag and a_tag.has_attr('href'):
-                    article_id = news_url + a_tag['href'].replace('./', '')
-                    date = tag_date.text.strip()
-                    title = tag_title.text.strip()
-                    if news_obj and (news_obj['id'] == article_id or date < news_obj['date']):
-                        break
-                    results.append(self.create_news_log_object(date, title, article_id))
-            success_count = 0
-            for result in reversed(results):
-                process_result = self.create_news_log_from_news_log_object(result)
-                if process_result == 0:
-                    success_count += 1
-            if len(results) > 0:
-                self.create_news_log_cache(success_count, results[0])
-        except Exception as e:
-            print("Error in running " + self.__class__.__name__ + ' - News')
-            print(e)
+        self.download_template_news(page_prefix=self.PAGE_PREFIX, article_select='ul.newsListsWrap li',
+                                    date_select='p.update_time', title_select='p.update_ttl',
+                                    id_select='a', a_tag_prefix=news_url, a_tag_start_text_to_remove='./')
 
     def download_key_visual(self):
         folder = self.create_key_visual_directory()
