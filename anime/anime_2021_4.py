eabb1fc2dde74fbc345db75710254d12bea83a5e
tetrix1993 <tetrix1993@hotmail.com>
2021-10-06 18:19:39 +0800

    Added ShuumatsuNoHarem Preview

--- anime/anime_2021_4.py
+++ anime/anime_2021_4.py
@@ -1256,7 +1256,30 @@ class ShuumatsuNoHaremDownload(Fall2021AnimeDownload, NewsTemplate2):
         self.download_media()
 
     def download_episode_preview(self):
-        self.has_website_updated(self.PAGE_PREFIX, 'index')
+        try:
+            soup = self.get_soup(self.PAGE_PREFIX + 'story/', decode=True)
+            a_tags = soup.select('table a')
+            for a_tag in a_tags:
+                if a_tag.has_attr('href'):
+                    try:
+                        episode = str(int(a_tag.text.strip().replace('第', '').replace('話', ''))).zfill(2)
+                    except Exception as e:
+                        continue
+                    if self.is_image_exists(episode + '_1'):
+                        continue
+                    url = self.PAGE_PREFIX + a_tag['href'].replace('../', '')
+                    ep_soup = self.get_soup(url)
+                    if ep_soup:
+                        images = ep_soup.select('ul.tp5 img')
+                        self.image_list = []
+                        for i in range(len(images)):
+                            image_url = self.PAGE_PREFIX + images[i]['src'].split('?')[0].replace('../', '')
+                            image_name = episode + '_' + str(i + 1)
+                            self.add_to_image_list(image_name, image_url)
+                        self.download_image_list(self.base_folder)
+        except Exception as e:
+            print("Error in running " + self.__class__.__name__)
+            print(e)
 
     def download_news(self):
         self.download_template_news(self.PAGE_PREFIX, 'news/list00010000.html')
