e5076b57a470581d28f2c01513b3e0943a982d28
tetrix1993 <tetrix1993@hotmail.com>
2022-01-05 17:32:10 +0800

    Added SlowLoop Preview

--- anime/anime_2022_1.py
+++ anime/anime_2022_1.py
@@ -1035,18 +1035,37 @@ class SlowLoopDownload(Winter2022AnimeDownload, NewsTemplate):
     folder_name = 'slow-loop'
 
     PAGE_PREFIX = website
+    FINAL_EPISODE = 12
+    IMAGES_PER_EPISODE = 6
 
     def __init__(self):
         super().__init__()
 
     def run(self):
         self.download_episode_preview()
+        self.download_episode_preview_external()
         self.download_news()
         self.download_key_visual()
         self.download_character()
 
     def download_episode_preview(self):
-        self.has_website_updated(self.PAGE_PREFIX, 'index')
+        template = self.PAGE_PREFIX + 'images/story/%s/p_%s.jpg'
+        for i in range(self.FINAL_EPISODE):
+            episode = str(i + 1).zfill(2)
+            if self.is_image_exists(episode + '_1'):
+                continue
+            ep_template = template % (str(i + 1).zfill(3), '%s')
+            for j in range(self.IMAGES_PER_EPISODE):
+                image_url = ep_template % str(j + 1).zfill(3)
+                image_name = episode + '_' + str(j + 1)
+                result = self.download_image(image_url, self.base_folder + '/' + image_name)
+                if result == -1:
+                    return
+
+    def download_episode_preview_external(self):
+        jp_title = 'スローループ'
+        AniverseMagazineScanner(jp_title, self.base_folder, last_episode=self.FINAL_EPISODE,
+                                end_date='20220105', download_id=self.download_id).run()
 
     def download_news(self):
         self.download_template_news(page_prefix=self.PAGE_PREFIX, news_prefix='news.html', article_select='article',
