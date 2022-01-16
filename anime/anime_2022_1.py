862e5230a0067ef5e2533602835cf2287680f43f
tetrix1993 <tetrix1993@hotmail.com>
2022-01-16 21:43:05 +0800

    Added ShuumatsuNoHarem Guess

--- anime/anime_2022_1.py
+++ anime/anime_2022_1.py
@@ -1180,6 +1180,8 @@ class ShuumatsuNoHaremDownload(Winter2022AnimeDownload, NewsTemplate2):
     folder_name = 'shuumatsu-no-harem'
 
     PAGE_PREFIX = website
+    FINAL_EPISODE = 12
+    IMAGES_PER_EPISODE = 6
 
     def __init__(self):
         super().__init__()
@@ -1187,6 +1189,7 @@ class ShuumatsuNoHaremDownload(Winter2022AnimeDownload, NewsTemplate2):
     def run(self):
         self.download_episode_preview()
         self.download_news()
+        self.download_episode_preview_guess()
         self.download_key_visual()
         self.download_character()
         self.download_media()
@@ -1215,6 +1218,37 @@ class ShuumatsuNoHaremDownload(Winter2022AnimeDownload, NewsTemplate2):
                         self.download_image_list(self.base_folder)
         except Exception as e:
             self.print_exception(e)
+
+    def download_episode_preview_guess(self):
+        folder = self.create_custom_directory('guess')
+        template = self.PAGE_PREFIX + 'core_sys/images/contents/%s/block/%s/%s.jpg'
+        is_successful = False
+        for i in range(self.FINAL_EPISODE):
+            episode = str(i + 1).zfill(2)
+            is_success = False
+            if self.is_image_exists(episode + '_1'):
+                continue
+            first = 38 + i
+            second = 149 + 6 * i
+            third = 108 + 6 * i
+            for j in range(self.IMAGES_PER_EPISODE):
+                image_url = template % (str(first).zfill(8), str(second).zfill(8), str(third + j).zfill(8))
+                image_name = episode + '_' + str(j + 1)
+                result = self.download_image(image_url, folder + '/' + image_name)
+                if result == 0:
+                    is_success = True
+                    is_successful = True
+                elif result == -1:
+                    break
+            if is_success:
+                print(self.__class__.__name__ + ' - Guessed successfully!')
+            else:
+                if len(os.listdir(folder)) == 0:
+                    os.rmdir(folder)
+                return
+        if len(os.listdir(folder)) == 0:
+            os.rmdir(folder)
+        return is_successful
 
     def download_news(self):
         self.download_template_news(self.PAGE_PREFIX, 'news/list00010000.html')
