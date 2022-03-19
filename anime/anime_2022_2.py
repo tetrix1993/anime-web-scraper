5315288b7e2d85d43fbfe83d9aba2532f7670917
tetrix1993 <tetrix1993@hotmail.com>
2022-03-19 09:15:26 +0800

    Added Yuuyame Preview

--- anime/anime_2022_2.py
+++ anime/anime_2022_2.py
@@ -1357,6 +1357,8 @@ class YuuyameDownload(Spring2022AnimeDownload, NewsTemplate):
     folder_name = 'yuuyame'
 
     PAGE_PREFIX = website
+    FINAL_EPISODE = 12
+    IMAGES_PER_EPISODE = 6
 
     def __init__(self):
         super().__init__()
@@ -1368,7 +1370,18 @@ class YuuyameDownload(Spring2022AnimeDownload, NewsTemplate):
         self.download_character()
 
     def download_episode_preview(self):
-        self.has_website_updated(self.PAGE_PREFIX, 'index')
+        template = self.PAGE_PREFIX + 'images/story/%s/p_%s.jpg'
+        for i in range(self.FINAL_EPISODE):
+            episode = str(i + 1).zfill(2)
+            if self.is_image_exists(episode + '_1'):
+                continue
+            ep_template = template % (str(i + 1).zfill(3), '%s')
+            for j in range(self.IMAGES_PER_EPISODE):
+                image_url = ep_template % str(j + 1).zfill(3)
+                image_name = episode + '_' + str(j + 1)
+                result = self.download_image(image_url, self.base_folder + '/' + image_name)
+                if result == -1:
+                    return
 
     def download_news(self):
         # Need update for paging
