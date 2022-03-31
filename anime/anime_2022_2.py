7002fba041249eaa38676f78512955c8f95e74ce
tetrix1993 <tetrix1993@hotmail.com>
2022-03-31 20:54:00 +0800

    Added Mobseka Preview

--- anime/anime_2022_2.py
+++ anime/anime_2022_2.py
@@ -1008,6 +1008,8 @@ class MobsekaDownload(Spring2022AnimeDownload, NewsTemplate):
     folder_name = 'mobseka'
 
     PAGE_PREFIX = website
+    FINAL_EPISODE = 12
+    IMAGES_PER_EPISODE = 6
 
     def __init__(self):
         super().__init__()
@@ -1015,11 +1017,25 @@ class MobsekaDownload(Spring2022AnimeDownload, NewsTemplate):
     def run(self):
         self.download_episode_preview()
         self.download_news()
+        self.download_episode_preview_external()
         self.download_key_visual()
         self.download_character()
 
     def download_episode_preview(self):
-        self.has_website_updated(self.PAGE_PREFIX, 'index')
+        image_url_template = self.PAGE_PREFIX + 'img/story/ep%s_img%s.jpg'
+        for i in range(self.FINAL_EPISODE):
+            for j in range(self.IMAGES_PER_EPISODE):
+                image_name = str(i + 1).zfill(2) + '_' + str(j + 1)
+                if not self.is_image_exists(image_name):
+                    image_url = image_url_template % (str(i + 1).zfill(2), str(j + 1).zfill(2))
+                    result = self.download_image(image_url, self.base_folder + '/' + image_name)
+                    if result == -1:
+                        return
+
+    def download_episode_preview_external(self):
+        jp_title = '⼄⼥ゲー世界はモブに厳しい世界です'
+        AniverseMagazineScanner(jp_title, self.base_folder, last_episode=self.FINAL_EPISODE,
+                                end_date='20220331', download_id=self.download_id).run()
 
     def download_news(self):
         self.download_template_news(page_prefix=self.PAGE_PREFIX, news_prefix='news.html', article_select='#news dl',
