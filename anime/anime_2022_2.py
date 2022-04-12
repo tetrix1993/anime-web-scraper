0ae6ba8ddc8c8765f1e902afecc16b2c1e1db113
tetrix1993 <tetrix1993@hotmail.com>
2022-04-12 20:49:28 +0800

    Added SummertimeRender Preview

--- anime/anime_2022_2.py
+++ anime/anime_2022_2.py
@@ -2209,7 +2209,27 @@ class SummertimeRenderDownload(Spring2022AnimeDownload, NewsTemplate):
         self.download_character()
 
     def download_episode_preview(self):
-        self.has_website_updated(self.PAGE_PREFIX, 'index')
+        try:
+            soup = self.get_soup(self.PAGE_PREFIX + 'story/')
+            a_tags = soup.select('.story--nav a[href]')
+            for a_tag in a_tags:
+                try:
+                    episode = str(int(a_tag.text)).zfill(2)
+                except:
+                    continue
+                if self.is_image_exists(episode + '_1'):
+                    continue
+                ep_soup = self.get_soup(a_tag['href'])
+                if ep_soup is not None:
+                    images = ep_soup.select('.story--main__slider img[src]')
+                    self.image_list = []
+                    for i in range(len(images)):
+                        image_url = images[i]['src'].split('?')[0]
+                        image_name = episode + '_' + str(i + 1)
+                        self.add_to_image_list(image_name, image_url)
+                    self.download_image_list(self.base_folder)
+        except Exception as e:
+            self.print_exception(e)
 
     def download_news(self):
         self.download_template_news(page_prefix=self.PAGE_PREFIX, article_select='.md-news__li',
