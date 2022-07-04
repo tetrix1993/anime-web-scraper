803f212b2a5bee6ec32daa8414e30b70daf7262b
tetrix1993 <tetrix1993@hotmail.com>
2022-07-05 00:19:09 +0800

    Added TenseiKenja Blu-ray

--- anime/anime_2022_3.py
+++ anime/anime_2022_3.py
@@ -1560,6 +1560,7 @@ class TenseiKenjaDownload(Summer2022AnimeDownload, NewsTemplate):
         self.download_news()
         self.download_key_visual()
         self.download_character()
+        self.download_media()
 
     def download_episode_preview(self):
         try:
@@ -1632,6 +1633,22 @@ class TenseiKenjaDownload(Summer2022AnimeDownload, NewsTemplate):
             self.download_image_list(folder)
         except Exception as e:
             self.print_exception(e, 'Character')
+
+    def download_media(self):
+        folder = self.create_media_directory()
+        bd_url = self.PAGE_PREFIX + 'product/'
+        for page in [('', 'Blu-ray'), ('oritoku/', 'Blu-ray Bonus')]:
+            try:
+                soup = self.get_soup(bd_url + page[0])
+                images = soup.select('.product-Section img[src]')
+                self.image_list = []
+                for image in images:
+                    image_url = image['src']
+                    image_name = self.extract_image_name_from_url(image_url)
+                    self.add_to_image_list(image_name, image_url)
+                self.download_image_list(folder)
+            except Exception as e:
+                self.print_exception(e, page[1])
 
 
 # Utawarerumono: Futari no Hakuoro
