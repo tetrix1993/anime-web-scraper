e5a2d55b841585ce63fa153ba9d7911e6546c24f
tetrix1993 <tetrix1993@hotmail.com>
2022-07-09 00:01:55 +0800

    Added KuronoShoukanshi Episode Visual

--- anime/anime_2022_3.py
+++ anime/anime_2022_3.py
@@ -1149,7 +1149,25 @@ class KuronoShoukanshiDownload(Summer2022AnimeDownload, NewsTemplate):
         self.download_character()
 
     def download_episode_preview(self):
-        self.has_website_updated(self.PAGE_PREFIX, 'index')
+        try:
+            soup = self.get_soup(self.PAGE_PREFIX + 'story')
+            boxes = soup.select('.str-Box_Inner')
+            for box in boxes:
+                try:
+                    episode = str(int(box.select('.number')[0].text.replace('#', ''))).zfill(2)
+                except:
+                    continue
+                if self.is_image_exists(episode + '_1'):
+                    continue
+                self.image_list = []
+                images = box.select('.swiper-slide img[src]')
+                for i in range(len(images)):
+                    image_url = images[i]['src']
+                    image_name = episode + '_' + str(i + 1)
+                    self.add_to_image_list(image_name, image_url)
+                self.download_image_list(self.base_folder)
+        except Exception as e:
+            self.print_exception(e)
 
     def download_news(self):
         self.download_template_news(page_prefix=self.PAGE_PREFIX, article_select='.news-item',
