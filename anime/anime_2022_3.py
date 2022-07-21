91683e405b5c92c471b957a4f5bca95e6ab8222a
tetrix1993 <tetrix1993@hotmail.com>
2022-07-21 21:01:25 +0800

    Added MaidgaAyashii Episode Preview

--- anime/anime_2022_3.py
+++ anime/anime_2022_3.py
@@ -1676,7 +1676,30 @@ class MaidgaAyashiiDownload(Summer2022AnimeDownload, NewsTemplate):
         self.download_character()
 
     def download_episode_preview(self):
-        self.has_website_updated(self.PAGE_PREFIX, 'index')
+        try:
+            soup = self.get_soup(self.PAGE_PREFIX + 'story/')
+            a_tags = soup.select('.story--nav a[href]')
+            for a_tag in a_tags:
+                try:
+                    episode = str(int(a_tag.text)).zfill(2)
+                except:
+                    continue
+                i_tag = soup.select('i.num')
+                if len(i_tag) > 0 and i_tag[0].text.strip() == episode:
+                    ep_soup = soup
+                else:
+                    ep_soup = self.get_soup(a_tag['href'])
+                if ep_soup is None:
+                    continue
+                self.image_list = []
+                images = ep_soup.select('.ss img[src]')
+                for i in range(len(images)):
+                    image_url = images[i]['src']
+                    image_name = episode + '_' + str(i + 1)
+                    self.add_to_image_list(image_name, image_url)
+                self.download_image_list(self.base_folder)
+        except Exception as e:
+            self.print_exception(e)
 
     def download_news(self):
         self.download_template_news(page_prefix=self.PAGE_PREFIX, article_select='.md-news--li',
