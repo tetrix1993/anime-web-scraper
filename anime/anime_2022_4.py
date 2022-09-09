353a76c6f7567a1e351d029db948bd2ffa143ba3
tetrix1993 <tetrix1993@hotmail.com>
2022-09-09 09:44:25 +0800

    Added BeastTamer Episode Preview

--- anime/anime_2022_4.py
+++ anime/anime_2022_4.py
@@ -986,6 +986,8 @@ class BeastTamerDownload(Fall2022AnimeDownload, NewsTemplate):
     folder_name = 'beast-tamer'
 
     PAGE_PREFIX = website
+    FINAL_EPISODE = 12
+    IMAGES_PER_EPISODE = 6
 
     def __init__(self):
         super().__init__()
@@ -997,7 +999,24 @@ class BeastTamerDownload(Fall2022AnimeDownload, NewsTemplate):
         self.download_character()
 
     def download_episode_preview(self):
-        self.has_website_updated(self.PAGE_PREFIX, 'index')
+        template = self.PAGE_PREFIX + 'assets/story/%s/%s.jpg'
+        try:
+            stop = False
+            for i in range(self.FINAL_EPISODE):
+                episode = str(i + 1).zfill(2)
+                if self.is_image_exists(episode + '_1'):
+                    continue
+                for j in range(self.IMAGES_PER_EPISODE):
+                    image_url = template % (str(i + 1), str(j + 1))
+                    image_name = episode + '_' + str(j + 1)
+                    result = self.download_image(image_url, self.base_folder + '/' + image_name)
+                    if result == -1:
+                        stop = True
+                        break
+                if stop:
+                    break
+        except Exception as e:
+            self.print_exception(e)
 
     def download_news(self):
         self.download_template_news(page_prefix=self.PAGE_PREFIX, article_select='article.news-atl-bg',
