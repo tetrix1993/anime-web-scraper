8a0ea49c99f9ade1c42c59a09b53f1e50dc8e337
tetrix1993 <tetrix1993@hotmail.com>
2022-09-23 22:23:44 +0800

    Added Shippona Episode Preview

--- anime/anime_2022_4.py
+++ anime/anime_2022_4.py
@@ -1223,6 +1223,8 @@ class ShipponaDownload(Fall2022AnimeDownload, NewsTemplate):
     folder_name = 'shippona'
 
     PAGE_PREFIX = website
+    FINAL_EPISODE = 12
+    IMAGES_PER_EPISODE = 5
 
     def __init__(self):
         super().__init__()
@@ -1234,7 +1236,16 @@ class ShipponaDownload(Fall2022AnimeDownload, NewsTemplate):
         self.download_character()
 
     def download_episode_preview(self):
-        self.has_website_updated(self.PAGE_PREFIX)
+        template = self.PAGE_PREFIX + 'story/img/%s/%s_%s.jpg'
+        for i in range(self.FINAL_EPISODE):
+            episode = str(i + 1).zfill(2)
+            if self.is_image_exists(episode + '_' + str(self.IMAGES_PER_EPISODE)):
+                continue
+            for j in range(self.IMAGES_PER_EPISODE):
+                image_url = template % (episode, episode, str(j + 1).zfill(2))
+                image_name = episode + '_' + str(j + 1)
+                if self.download_image(image_url, self.base_folder + '/' + image_name) == -1:
+                    return
 
     def download_news(self):
         self.download_template_news(page_prefix=self.PAGE_PREFIX, article_select='a.news-list-item',
