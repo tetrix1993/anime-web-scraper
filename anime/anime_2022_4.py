4a06002a5aa1af8b73f668ff4c56614081c85e75
tetrix1993 <tetrix1993@hotmail.com>
2022-09-30 21:06:26 +0800

    Added Fuukoi Episode Preview

--- anime/anime_2022_4.py
+++ anime/anime_2022_4.py
@@ -1,7 +1,7 @@
 import os
 from anime.constants import HTTP_HEADER_USER_AGENT
 from anime.main_download import MainDownload, NewsTemplate, NewsTemplate2
-from scan import EeoMediaScanner
+from scan import AniverseMagazineScanner, EeoMediaScanner
 
 
 # Akiba Meido Sensou https://akibamaidwar.com/ #アキバ冥途戦争 @akbmaidwar
@@ -611,18 +611,52 @@ class FuukoiDownload(Fall2022AnimeDownload, NewsTemplate2):
     folder_name = 'fuukoi'
 
     PAGE_PREFIX = website
+    FINAL_EPISODE = 12
 
     def __init__(self):
         super().__init__()
 
     def run(self):
         self.download_episode_preview()
+        self.download_episode_preview_external()
         self.download_news()
         self.download_key_visual()
         self.download_character()
 
     def download_episode_preview(self):
-        self.has_website_updated(self.PAGE_PREFIX, 'index')
+        try:
+            soup = self.get_soup(self.PAGE_PREFIX + 'story/01.html')
+            trs = soup.select('#ContentsListUnit02 tr[class]')
+            for tr in trs:
+                a_tags = tr.select('a[href]')
+                if len(a_tags) == 0:
+                    continue
+                try:
+                    episode = str(int(a_tags[0].text.replace('#', ''))).zfill(2)
+                except:
+                    continue
+                if self.is_image_exists(episode + '_1'):
+                    continue
+                if episode == '01':
+                    ep_soup = soup
+                else:
+                    ep_soup = self.get_soup(self.PAGE_PREFIX + a_tags[0]['href'].replace('../', ''))
+                if ep_soup is None:
+                    continue
+                self.image_list = []
+                images = ep_soup.select('ul.tp5 img[src]')
+                for i in range(len(images)):
+                    image_url = self.PAGE_PREFIX + images[i]['src'].split('?')[0].replace('../', '').replace('sn_', '')
+                    image_name = episode + '_' + str(i + 1)
+                    self.add_to_image_list(image_name, image_url)
+                self.download_image_list(self.base_folder)
+        except Exception as e:
+            self.print_exception(e)
+
+    def download_episode_preview_external(self):
+        jp_title = '夫婦以上、恋人未満'
+        AniverseMagazineScanner(jp_title, self.base_folder, last_episode=self.FINAL_EPISODE,
+                                end_date='20220930', download_id=self.download_id).run()
 
     def download_news(self):
         self.download_template_news(self.PAGE_PREFIX)
