7ea7200e35f9d748dc45b36e7e6fd9424b8b6a7d
tetrix1993 <tetrix1993@hotmail.com>
2022-10-30 19:18:11 +0800

    Added Fuukoi Guess

--- anime/anime_2022_4.py
+++ anime/anime_2022_4.py
@@ -871,6 +871,7 @@ class FuukoiDownload(Fall2022AnimeDownload, NewsTemplate2):
 
     PAGE_PREFIX = website
     FINAL_EPISODE = 12
+    IMAGES_PER_EPISODE = 6
 
     def __init__(self):
         super().__init__()
@@ -878,6 +879,7 @@ class FuukoiDownload(Fall2022AnimeDownload, NewsTemplate2):
     def run(self):
         self.download_episode_preview()
         self.download_episode_preview_external()
+        self.download_episode_preview_guess()
         self.download_news()
         self.download_key_visual()
         self.download_character()
@@ -917,6 +919,37 @@ class FuukoiDownload(Fall2022AnimeDownload, NewsTemplate2):
         jp_title = '夫婦以上、恋人未満'
         AniverseMagazineScanner(jp_title, self.base_folder, last_episode=self.FINAL_EPISODE,
                                 end_date='20220930', download_id=self.download_id).run()
+
+    def download_episode_preview_guess(self):
+        folder = self.create_custom_directory('guess')
+        template = self.PAGE_PREFIX + 'core_sys/images/contents/%s/block/%s/%s.jpg'
+        is_successful = False
+        for i in range(5, self.FINAL_EPISODE + 1, 1):
+            episode = str(i).zfill(2)
+            is_success = False
+            if self.is_image_exists(episode + '_1'):
+                continue
+            first = 38 + (i - 5)
+            second = 105 + 8 * (i - 5)
+            third = 157 + 12 * (i - 5)
+            for j in range(self.IMAGES_PER_EPISODE):
+                image_url = template % (str(first).zfill(8), str(second).zfill(8), str(third + j).zfill(8))
+                image_name = episode + '_' + str(j + 1)
+                result = self.download_image(image_url, folder + '/' + image_name)
+                if result == 0:
+                    is_success = True
+                    is_successful = True
+                elif result == -1:
+                    break
+            if is_success:
+                print(self.__class__.__name__ + ' - Guessed successfully!')
+            else:
+                if len(os.listdir(folder)) == 0:
+                    os.rmdir(folder)
+                return
+        if len(os.listdir(folder)) == 0:
+            os.rmdir(folder)
+        return is_successful
 
     def download_news(self):
         self.download_template_news(self.PAGE_PREFIX)
