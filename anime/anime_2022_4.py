2e6a7780d5474398285d6e96e16c7d7df669d5d5
tetrix1993 <tetrix1993@hotmail.com>
2022-11-04 00:31:25 +0800

    Updated KanColle2 Character

--- anime/anime_2022_4.py
+++ anime/anime_2022_4.py
@@ -1246,7 +1246,7 @@ class KagenoJitsuryokushaDownload(Fall2022AnimeDownload, NewsTemplate):
 # KanColle: Itsuka Ano Umi de
 class KanColle2Download(Fall2022AnimeDownload, NewsTemplate2):
     title = 'KanColle: Itsuka Ano Umi de'
-    keywords = [title]
+    keywords = [title, 'Kantai Collection', 'KanColle Season 2: Let\'s Meet at Sea']
     website = 'https://kancolle-itsuumi.com/'
     twitter = 'anime_KanColle'
     hashtags = ['艦これ', 'いつかあの海で']
@@ -1262,6 +1262,7 @@ class KanColle2Download(Fall2022AnimeDownload, NewsTemplate2):
         self.download_episode_preview()
         self.download_news()
         self.download_key_visual()
+        self.download_character()
 
     def download_episode_preview(self):
         try:
@@ -1301,6 +1302,40 @@ class KanColle2Download(Fall2022AnimeDownload, NewsTemplate2):
             self.download_image_list(folder)
         except Exception as e:
             self.print_exception(e, 'Key Visual')
+
+    def download_character(self):
+        folder = self.create_character_directory()
+        cache_filepath = folder + '/cache'
+        processed, num_processed = self.get_processed_items_from_cache_file(cache_filepath)
+        try:
+            soup = self.get_soup(self.PAGE_PREFIX + 'chara/')
+            a_tags = soup.select('#ContentsListUnit01 a[href]')
+            for a_tag in a_tags:
+                if not a_tag['href'].endswith('.html'):
+                    continue
+                page = a_tag['href'].split('/')[-1].split('.html')[0]
+                if page in processed:
+                    continue
+                if page == 'index':
+                    chara_soup = soup
+                else:
+                    chara_soup = self.get_soup(self.PAGE_PREFIX + a_tag['href'].replace('../', ''))
+                if chara_soup is None:
+                    continue
+                self.image_list = []
+                images = chara_soup.select('.chara__stand img[src],.chara__face img[src]')
+                for image in images:
+                    if '/chara/' not in image['src']:
+                        continue
+                    image_url = self.PAGE_PREFIX + image['src'].replace('../', '')
+                    image_name = self.generate_image_name_from_url(image_url, 'chara')
+                    self.add_to_image_list(image_name, image_url)
+                if len(self.image_list) > 0:
+                    processed.append(page)
+                self.download_image_list(folder)
+        except Exception as e:
+            self.print_exception(e, 'Character')
+        self.create_cache_file(cache_filepath, processed, num_processed)
 
 
 # Kidou Senshi Gundam: Suisei no Majo
