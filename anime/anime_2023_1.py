e980d8d1f3c1b690a27ff4fd78d2c9ca3c19e4c6
tetrix1993 <tetrix1993@hotmail.com>
2022-12-17 10:18:41 +0800

    Added Bofuri2 Blu-ray

--- anime/anime_2023_1.py
+++ anime/anime_2023_1.py
@@ -462,6 +462,7 @@ class Bofuri2Download(Winter2023AnimeDownload):
         self.download_news()
         self.download_key_visual()
         self.download_character()
+        self.download_media()
 
     def download_episode_preview(self):
         self.has_website_updated(self.PAGE_PREFIX, 'index')
@@ -512,6 +513,37 @@ class Bofuri2Download(Winter2023AnimeDownload):
         folder = self.create_character_directory()
         template = self.PAGE_PREFIX + 'assets/character/%s.png'
         self.download_by_template(folder, template, 1, 1)
+
+    def download_media(self):
+        folder = self.create_media_directory()
+
+        # Blu-ray
+        try:
+            self.image_list = []
+            soup = self.get_soup(self.PAGE_PREFIX + 'bluray/')
+            images = soup.select('.bd-img img[src]')
+            for image in images:
+                if '/bluray/' in image['src'] and not image['src'].endswith('/np.png'):
+                    image_url = self.PAGE_PREFIX + image['src'].replace('../', '')
+                    image_name = self.generate_image_name_from_url(image_url, 'bluray')
+                    self.add_to_image_list(image_name, image_url)
+            self.download_image_list(folder)
+        except Exception as e:
+            self.print_exception(e, 'Blu-ray')
+
+        # Blu-ray Bonus
+        try:
+            self.image_list = []
+            soup = self.get_soup(self.PAGE_PREFIX + 'bluray/campaign.html')
+            images = soup.select('.bd-cp-img img[src], .bd-shopbnf-img img[src]')
+            for image in images:
+                if '/bluray/' in image['src'] and not image['src'].endswith('/np.png'):
+                    image_url = self.PAGE_PREFIX + image['src'].replace('../', '')
+                    image_name = self.generate_image_name_from_url(image_url, 'bluray')
+                    self.add_to_image_list(image_name, image_url)
+            self.download_image_list(folder)
+        except Exception as e:
+            self.print_exception(e, 'Blu-ray Bonus')
 
 
 # Kaiko sareta Ankoku Heishi (30-dai) no Slow na Second Life
