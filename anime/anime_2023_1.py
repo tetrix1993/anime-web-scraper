3b39b679f98a459ed617d80aff9db184c124f09a
tetrix1993 <tetrix1993@hotmail.com>
2023-01-01 22:00:15 +0800

    Added NierAutomata Episode Preview

--- anime/anime_2023_1.py
+++ anime/anime_2023_1.py
@@ -1167,7 +1167,38 @@ class NierAutomataDownload(Winter2023AnimeDownload, NewsTemplate):
         self.download_key_visual()
 
     def download_episode_preview(self):
-        self.has_website_updated(self.PAGE_PREFIX)
+        story_url = self.PAGE_PREFIX + 'story/'
+        try:
+            soup = self.get_soup(story_url, decode=True)
+            stories = soup.select('.p-story_nav__list-item')
+            for story in stories:
+                a_tags = story.select('a[href]')
+                if len(a_tags) < 1:
+                    continue
+                try:
+                    ep_num_tag = a_tags[0].select('.p-story_nav__link-text')
+                    ep_num = int(ep_num_tag[0].text)
+                    if ep_num is None or ep_num < 1:
+                        continue
+                    episode = str(ep_num).zfill(2)
+                except:
+                    continue
+                if self.is_image_exists(episode + '_1'):
+                    continue
+                if story.has_attr('class') and 'is-current' in story['class']:
+                    ep_soup = soup
+                else:
+                    ep_soup = self.get_soup(story_url + a_tags[0]['href'].replace('./', ''))
+                if ep_soup is not None:
+                    images = ep_soup.select('.p-story_visual__data-img img[src]')
+                    self.image_list = []
+                    for i in range(len(images)):
+                        image_url = story_url + images[i]['src']
+                        image_name = episode + '_' + str(i + 1)
+                        self.add_to_image_list(image_name, image_url)
+                    self.download_image_list(self.base_folder)
+        except Exception as e:
+            self.print_exception(e)
 
     def download_news(self):
         self.download_template_news(page_prefix=self.PAGE_PREFIX, article_select='.p-news__list-item',
