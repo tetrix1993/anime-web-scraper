f406b7fc8b6737f3037e883bb7b503248a255fc8
tetrix1993 <tetrix1993@hotmail.com>
2023-01-02 19:20:50 +0800

    Added Tomochan Episode Preview

--- anime/anime_2023_1.py
+++ anime/anime_2023_1.py
@@ -1973,7 +1973,37 @@ class TomochanDownload(Winter2023AnimeDownload, NewsTemplate):
         self.download_media()
 
     def download_episode_preview(self):
-        self.has_website_updated(self.PAGE_PREFIX)
+        story_url = self.PAGE_PREFIX + 'story/'
+        try:
+            soup = self.get_soup(story_url, decode=True)
+            stories = soup.select('.p-story__nav li')
+            for story in stories:
+                a_tags = story.select('a[href]')
+                if len(a_tags) == 0:
+                    continue
+                try:
+                    ep_num = int(a_tags[0].text.replace('#', ''))
+                    if ep_num is None or ep_num < 1:
+                        continue
+                    episode = str(ep_num).zfill(2)
+                except:
+                    continue
+                if self.is_image_exists(episode + '_1'):
+                    continue
+                if story.has_attr('class') and 'is-current' in story['class']:
+                    ep_soup = soup
+                else:
+                    ep_soup = self.get_soup(story_url + a_tags[0]['href'].replace('./', ''))
+                if ep_soup is not None:
+                    images = ep_soup.select('.swiper-wrapper img[src]')
+                    self.image_list = []
+                    for i in range(len(images)):
+                        image_url = story_url + images[i]['src']
+                        image_name = episode + '_' + str(i + 1)
+                        self.add_to_image_list(image_name, image_url)
+                    self.download_image_list(self.base_folder)
+        except Exception as e:
+            self.print_exception(e)
 
     def download_news(self):
         self.download_template_news(page_prefix=self.PAGE_PREFIX, article_select='li.p-news__list-item',
