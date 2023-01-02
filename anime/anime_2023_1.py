7117b03e5ee557e2c7ff0a7a8481cde8d677d971
tetrix1993 <tetrix1993@hotmail.com>
2023-01-02 20:07:34 +0800

    Added AnkokuHeishi Episode Preview

--- anime/anime_2023_1.py
+++ anime/anime_2023_1.py
@@ -770,7 +770,31 @@ class AnkokuHeishiDownload(Winter2023AnimeDownload, NewsTemplate):
         self.download_media()
 
     def download_episode_preview(self):
-        self.has_website_updated(self.PAGE_PREFIX)
+        try:
+            soup = self.get_soup(self.PAGE_PREFIX + 'story/story01/')
+            stories = soup.select('.story_nav a[href]')
+            for story in stories:
+                try:
+                    episode = str(int(story.text)).zfill(2)
+                except:
+                    continue
+                if self.is_image_exists(episode + '_1'):
+                    continue
+                if story.has_attr('class') and 'active' in story['class']:
+                    ep_soup = soup
+                else:
+                    ep_soup = soup.select(story['href'])
+                if ep_soup is None:
+                    continue
+                images = ep_soup.select('.wp-swiper__slide-content img[src]')
+                self.image_list = []
+                for i in range(len(images)):
+                    image_url = self.clear_resize_in_url(images[i]['src'])
+                    image_name = episode + '_' + str(i + 1)
+                    self.add_to_image_list(image_name, image_url)
+                self.download_image_list(self.base_folder)
+        except Exception as e:
+            self.print_exception(e)
 
     def download_news(self):
         self.download_template_news(page_prefix=self.PAGE_PREFIX, article_select='.news_item',
