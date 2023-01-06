5aba78e8bf40a6794a43b5099809805d77c3e118
tetrix1993 <tetrix1993@hotmail.com>
2023-01-06 09:38:43 +0800

    Added SugarApple Blu-ray

--- anime/anime_2023_1.py
+++ anime/anime_2023_1.py
@@ -2280,6 +2280,7 @@ class SugarAppleDownload(Winter2023AnimeDownload, NewsTemplate):
         self.download_news()
         self.download_key_visual()
         self.download_character()
+        self.download_media()
 
     def download_episode_preview(self):
         template = self.PAGE_PREFIX + 'story/img/ep%s_img%s.jpg'
@@ -2323,6 +2324,25 @@ class SugarAppleDownload(Winter2023AnimeDownload, NewsTemplate):
         folder = self.create_character_directory()
         template = self.PAGE_PREFIX + 'img/chara/body_%s.png'
         self.download_by_template(folder, template, 2, 1)
+
+    def download_media(self):
+        folder = self.create_media_directory()
+        try:
+            soup = self.get_soup(self.PAGE_PREFIX + 'bluray/')
+            images = soup.select('.page_wrapper img[src]')
+            self.image_list = []
+            for image in images:
+                if '/bluray/' not in image['src']:
+                    continue
+                temp_name = image['src'].split('/')[-1]
+                if temp_name.startswith('img_cs_'):
+                    continue
+                image_url = self.PAGE_PREFIX + image['src'].replace('../', '')
+                image_name = self.generate_image_name_from_url(image_url, 'bluray')
+                self.add_to_image_list(image_name, image_url)
+            self.download_image_list(folder)
+        except Exception as e:
+            self.print_exception(e, 'Blu-ray')
 
 
 # Tensei Oujo to Tensai Reijou no Mahou Kakumei
