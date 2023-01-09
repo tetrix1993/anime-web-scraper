ce827f26f123f1d5425d0e1c21ec1c63d88744ed
tetrix1993 <tetrix1993@hotmail.com>
2023-01-10 07:49:15 +0800

    Added Mononogatari Blu-ray

--- anime/anime_2023_1.py
+++ anime/anime_2023_1.py
@@ -1872,6 +1872,7 @@ class MononogatariDownload(Winter2023AnimeDownload):
         self.download_episode_preview()
         self.download_news()
         self.download_key_visual()
+        self.download_media()
 
     def download_episode_preview(self):
         try:
@@ -1950,6 +1951,22 @@ class MononogatariDownload(Winter2023AnimeDownload):
         self.image_list = []
         self.add_to_image_list('kv_aniverse', 'https://aniverse-mag.com/wp-content/uploads/2022/12/sub5-2.jpg')
         self.download_image_list(folder)
+
+    def download_media(self):
+        folder = self.create_media_directory()
+        try:
+            soup = self.get_soup(self.PAGE_PREFIX + 'blu-ray/')
+            self.image_list = []
+            images = soup.select('#Blu-ray img[src]')
+            for image in images:
+                if '/blu-ray/' not in image['src']:
+                    continue
+                image_url = self.PAGE_PREFIX + image['src'].replace('../', '')
+                image_name = self.generate_image_name_from_url(image_url, 'blu-ray')
+                self.add_to_image_list(image_name, image_url)
+            self.download_image_list(folder)
+        except Exception as e:
+            self.print_exception(e, 'Blu-ray')
             
 
 # NieR:Automata Ver1.1a
