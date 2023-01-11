9c823417ab14823d1b6ebb9a08eb7c14807c5312
tetrix1993 <tetrix1993@hotmail.com>
2023-01-12 00:50:00 +0800

    Added Spyroom Guess

--- anime/anime_2023_1.py
+++ anime/anime_2023_1.py
@@ -2726,6 +2726,7 @@ class SpyroomDownload(Winter2023AnimeDownload, NewsTemplate2):
 
     PAGE_PREFIX = website
     FINAL_EPISODE = 12
+    IMAGES_PER_EPISODE = 6
 
     def __init__(self):
         super().__init__()
@@ -2733,6 +2734,7 @@ class SpyroomDownload(Winter2023AnimeDownload, NewsTemplate2):
     def run(self):
         self.download_episode_preview()
         self.download_episode_preview_external()
+        self.download_episode_preview_guess()
         self.download_news()
         self.download_key_visual()
         self.download_character()
@@ -2772,6 +2774,37 @@ class SpyroomDownload(Winter2023AnimeDownload, NewsTemplate2):
         keywords = ['スパイ教室', 'カット']
         AniverseMagazineScanner(keywords, self.base_folder, last_episode=self.FINAL_EPISODE,
                                 end_date='20230104', prefix='#', suffix='', download_id=self.download_id).run()
+
+    def download_episode_preview_guess(self):
+        folder = self.create_custom_directory('guess')
+        template = self.PAGE_PREFIX + 'core_sys/images/contents/%s/block/%s/%s.jpg'
+        is_successful = False
+        for i in range(self.FINAL_EPISODE):
+            episode = str(i + 1).zfill(2)
+            is_success = False
+            if self.is_image_exists(episode + '_1'):
+                continue
+            first = 20 + i
+            second = 46 + 4 * i
+            third = 62 + self.IMAGES_PER_EPISODE * i
+            for j in range(self.IMAGES_PER_EPISODE):
+                image_url = template % (str(first).zfill(8), str(second).zfill(8), str(third + j).zfill(8))
+                image_name = episode + '_' + str(j + 1)
+                result = self.download_image(image_url, folder + '/' + image_name)
+                if result == 0:
+                    is_success = True
+                    is_successful = True
+                elif result == -1:
+                    break
+            if is_success:
+                print(self.__class__.__name__ + ' - Guessed successfully!')
+            else:
+                if len(os.listdir(folder)) == 0:
+                    os.rmdir(folder)
+                return
+        if len(os.listdir(folder)) == 0:
+            os.rmdir(folder)
+        return is_successful
 
     def download_news(self):
         self.download_template_news(self.PAGE_PREFIX)
