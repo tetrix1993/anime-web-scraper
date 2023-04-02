91368582544a6d21a28dea2a220048ef36d7ccdc
tetrix1993 <tetrix1993@hotmail.com>
2023-04-03 00:28:55 +0800

    Added Kimisomu Episode Preview

--- anime/anime_2023_2.py
+++ anime/anime_2023_2.py
@@ -929,6 +929,8 @@ class KimisomuDownload(Spring2023AnimeDownload, NewsTemplate):
     folder_name = 'kimisomu'
 
     PAGE_PREFIX = website
+    FINAL_EPISODE = 12
+    IMAGES_PER_EPISODE = 5
 
     def __init__(self):
         super().__init__()
@@ -940,7 +942,23 @@ class KimisomuDownload(Spring2023AnimeDownload, NewsTemplate):
         self.download_character()
 
     def download_episode_preview(self):
-        self.has_website_updated(self.PAGE_PREFIX)
+        try:
+            template = self.PAGE_PREFIX + 'wp/wp-content/themes/insomnia_v0/assets/images/common/story/story_%s-%s.png'
+            stop = False
+            for i in range(self.FINAL_EPISODE):
+                episode = str(i + 1).zfill(2)
+                if self.is_image_exists(episode + '_' + str(self.IMAGES_PER_EPISODE)):
+                    continue
+                for j in range(self.IMAGES_PER_EPISODE):
+                    image_url = template % (str(i + 1), str(j + 1))
+                    image_name = episode + '_' + str(j + 1)
+                    if self.download_image(image_url, self.base_folder + '/' + image_name) == -1:
+                        stop = True
+                        break
+                if stop:
+                    break
+        except Exception as e:
+            self.print_exception(e)
 
     def download_news(self):
         self.download_template_news(page_prefix=self.PAGE_PREFIX, article_select='.news-List_Item',
