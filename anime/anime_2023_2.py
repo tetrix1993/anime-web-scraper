98ea71b2a57ab97cf2a551efb8ffa4b8afdda3c8
tetrix1993 <tetrix1993@hotmail.com>
2023-04-08 17:34:09 +0800

    Added Guiguikuru Episode Preview

--- anime/anime_2023_2.py
+++ anime/anime_2023_2.py
@@ -816,7 +816,31 @@ class GuiguikuruDownload(Spring2023AnimeDownload, NewsTemplate):
         self.download_character()
 
     def download_episode_preview(self):
-        self.has_website_updated(self.PAGE_PREFIX)
+        try:
+            soup = self.get_soup(self.PAGE_PREFIX + 'story/ep01/')
+            stories = soup.select('.story--nav a[href]')
+            for story in stories:
+                try:
+                    episode = str(int(story.text)).zfill(2)
+                except:
+                    continue
+                if self.is_image_exists(episode + '_1'):
+                    continue
+                if episode == '01':
+                    ep_soup = soup
+                else:
+                    ep_soup = self.get_soup(story['href'])
+                if ep_soup is None:
+                    continue
+                self.image_list = []
+                images = ep_soup.select('.story img[src]')
+                for i in range(len(images)):
+                    image_url = images[i]['src']
+                    image_name = episode + '_' + str(i + 1)
+                    self.add_to_image_list(image_name, image_url)
+                self.download_image_list(self.base_folder)
+        except Exception as e:
+            self.print_exception(e)
 
     def download_news(self):
         self.download_template_news(page_prefix=self.PAGE_PREFIX, article_select='.md-list__news li',
