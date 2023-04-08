c19ccf544291dc40f051c3ae785d128863a18411
tetrix1993 <tetrix1993@hotmail.com>
2023-04-08 17:22:51 +0800

    Added OneKillSister Episode Preview

--- anime/anime_2023_2.py
+++ anime/anime_2023_2.py
@@ -460,7 +460,7 @@ class IseleveDownload(Spring2023AnimeDownload, NewsTemplate):
     folder_name = 'iseleve'
 
     PAGE_PREFIX = website
-    FINAL_EPISODE = 12
+    FINAL_EPISODE = 13
     IMAGES_PER_EPISODE = 6
 
     def __init__(self):
@@ -565,6 +565,8 @@ class OneKillSisterDownload(Spring2023AnimeDownload, NewsTemplate):
     folder_name = 'onekillsister'
 
     PAGE_PREFIX = website
+    FINAL_EPISODE = 12
+    IMAGES_PER_EPISODE = 10
 
     def __init__(self):
         super().__init__()
@@ -576,7 +578,23 @@ class OneKillSisterDownload(Spring2023AnimeDownload, NewsTemplate):
         self.download_character()
 
     def download_episode_preview(self):
-        self.has_website_updated(self.PAGE_PREFIX, 'index')
+        try:
+            template = self.PAGE_PREFIX + 'wp-content/themes/otks/assets/img/story/%s/okn%s_%s.jpg'
+            stop = False
+            for i in range(self.FINAL_EPISODE):
+                episode = str(i + 1).zfill(2)
+                if self.is_image_exists(episode + '_' + str(self.IMAGES_PER_EPISODE).zfill(2)):
+                    continue
+                for j in range(self.IMAGES_PER_EPISODE):
+                    image_url = template % (episode, episode, str(j + 1).zfill(2))
+                    image_name = episode + '_' + str(j + 1).zfill(2)
+                    if self.download_image(image_url, self.base_folder + '/' + image_name) == -1:
+                        stop = True
+                        break
+                if stop:
+                    break
+        except Exception as e:
+            self.print_exception(e)
 
     def download_news(self):
         self.download_template_news(page_prefix=self.PAGE_PREFIX, article_select='.news-list__item',
