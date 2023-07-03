b92989a713f9e8bbfc790d72440da34d75a83279
tetrix1993 <tetrix1993@hotmail.com>
2023-07-03 20:12:14 +0800

    Added Zom100 Episode Preview

--- anime/anime_2023_3.py
+++ anime/anime_2023_3.py
@@ -2239,6 +2239,8 @@ class Zom100Download(Summer2023AnimeDownload, NewsTemplate):
     folder_name = 'zom100'
 
     PAGE_PREFIX = website
+    FINAL_EPISODE = 12
+    IMAGES_PER_EPISODE = 4
 
     def __init__(self):
         super().__init__()
@@ -2250,7 +2252,23 @@ class Zom100Download(Summer2023AnimeDownload, NewsTemplate):
         self.download_character()
 
     def download_episode_preview(self):
-        self.has_website_updated(self.PAGE_PREFIX, 'index')
+        try:
+            template = self.PAGE_PREFIX + 'assets/story/%s/%s.jpg'
+            stop = False
+            for i in range(self.FINAL_EPISODE):
+                episode = str(i + 1).zfill(2)
+                if self.is_image_exists(episode + '_' + str(self.IMAGES_PER_EPISODE)):
+                    continue
+                for j in range(self.IMAGES_PER_EPISODE):
+                    image_url = template % (str(i + 1), str(j + 1))
+                    image_name = episode + '_' + str(j + 1)
+                    if self.download_image(image_url, self.base_folder + '/' + image_name) == -1:
+                        stop = True
+                        break
+                if stop:
+                    break
+        except Exception as e:
+            self.print_exception(e)
 
     def download_news(self):
         self.download_template_news(page_prefix=self.PAGE_PREFIX, article_select='.content-entry',
