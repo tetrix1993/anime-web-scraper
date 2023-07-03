f6c92803a32be8a31034eb9e3cc586fa06fc01f4
tetrix1993 <tetrix1993@hotmail.com>
2023-07-03 09:25:53 +0800

    Added Lv1Room Blu-ray

--- anime/anime_2023_3.py
+++ anime/anime_2023_3.py
@@ -746,6 +746,7 @@ class Lv1RoomDownload(Summer2023AnimeDownload, NewsTemplate2):
         self.download_news()
         self.download_key_visual()
         self.download_character()
+        self.download_media()
 
     def download_episode_preview(self):
         try:
@@ -833,6 +834,36 @@ class Lv1RoomDownload(Summer2023AnimeDownload, NewsTemplate2):
                     self.download_image_list(folder)
         except Exception as e:
             self.print_exception(e, 'Character')
+        self.create_cache_file(cache_filepath, processed, num_processed)
+
+    def download_media(self):
+        folder = self.create_media_directory()
+        cache_filepath = folder + '/cache'
+        processed, num_processed = self.get_processed_items_from_cache_file(cache_filepath)
+        for page in ['privilege', 'campaign', '01', '02', '03']:
+            try:
+                if page in processed:
+                    continue
+                page_url = self.PAGE_PREFIX + 'bddvd/'
+                if page != '01':
+                    page_url += page + '.html'
+                soup = self.get_soup(page_url)
+                images = soup.select('.block_inner img[src]')
+                self.image_list = []
+                for image in images:
+                    image_url = self.PAGE_PREFIX + image['src'].replace('../', '').split('?')[0]
+                    if '/bddvd/' in image_url:
+                        image_name = self.generate_image_name_from_url(image_url, 'bddvd')
+                    else:
+                        image_name = self.extract_image_name_from_url(image_url)
+                    if page.isnumeric() and not self.is_content_length_in_range(image_url, more_than_amount=10000):
+                        continue
+                    self.add_to_image_list(image_name, image_url)
+                if not page.isnumeric() or len(self.image_list) > 2:
+                    processed.append(page)
+                self.download_image_list(folder)
+            except Exception as e:
+                self.print_exception(e, f'Blu-ray - {page}')
         self.create_cache_file(cache_filepath, processed, num_processed)
 
 
