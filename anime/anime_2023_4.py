3cad000d5cf3c33cd45978085be60d288aa14825
tetrix1993 <tetrix1993@hotmail.com>
2023-09-19 22:38:49 +0800

    Added ToaruOssan Episode Preview

--- anime/anime_2023_4.py
+++ anime/anime_2023_4.py
@@ -1891,6 +1891,8 @@ class ToaruOssanDownload(Fall2023AnimeDownload, NewsTemplate):
     folder_name = 'toaruossan'
 
     PAGE_PREFIX = website
+    FINAL_EPISODE = 12
+    IMAGES_PER_EPISODE = 9
 
     def __init__(self):
         super().__init__()
@@ -1902,12 +1904,29 @@ class ToaruOssanDownload(Fall2023AnimeDownload, NewsTemplate):
         self.download_character()
 
     def download_episode_preview(self):
-        self.has_website_updated(self.PAGE_PREFIX, 'index')
+        try:
+            template = self.PAGE_PREFIX + 'assets/images/story/%s_%s.jpg'
+            stop = False
+            for i in range(self.FINAL_EPISODE):
+                episode = str(i + 1).zfill(2)
+                if self.is_image_exists(episode + '_' + str(self.IMAGES_PER_EPISODE)):
+                    continue
+                for j in range(self.IMAGES_PER_EPISODE):
+                    image_url = template % (episode, str(j + 1).zfill(2))
+                    image_name = episode + '_' + str(j + 1)
+                    if self.download_image(image_url, self.base_folder + '/' + image_name) == -1:
+                        stop = True
+                        break
+                if stop:
+                    break
+        except Exception as e:
+            self.print_exception(e)
 
     def download_news(self):
         news_url = self.PAGE_PREFIX + 'news/'
         self.download_template_news(page_prefix=self.PAGE_PREFIX, article_select='#news li',
-                                    date_select='time', title_select='p', id_select='a', a_tag_prefix=news_url)
+                                    date_select='time', title_select='p', id_select='a', a_tag_prefix=news_url,
+                                    a_tag_start_text_to_remove='../')
 
     def download_key_visual(self):
         folder = self.create_key_visual_directory()
