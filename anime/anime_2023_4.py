dfc840797ba851483cda8285447722dd0418053c
tetrix1993 <tetrix1993@hotmail.com>
2023-09-29 17:43:17 +0800

    Added DekobokoMajo Episode Preview

--- anime/anime_2023_4.py
+++ anime/anime_2023_4.py
@@ -351,6 +351,8 @@ class DekobokoMajoDownload(Fall2023AnimeDownload, NewsTemplate):
     folder_name = 'dekoboko-majo'
 
     PAGE_PREFIX = website
+    FINAL_EPISODE = 12
+    IMAGES_PER_EPISODE = 4
 
     def __init__(self):
         super().__init__()
@@ -363,7 +365,23 @@ class DekobokoMajoDownload(Fall2023AnimeDownload, NewsTemplate):
         self.download_media()
 
     def download_episode_preview(self):
-        self.has_website_updated(self.PAGE_PREFIX, 'index')
+        try:
+            template = self.PAGE_PREFIX + 'assets/images/story/%s_%s.jpg'
+            stop = False
+            for i in range(self.FINAL_EPISODE):
+                episode = str(i + 1).zfill(2)
+                if self.is_image_exists(episode + '_' + str(self.IMAGES_PER_EPISODE)):
+                    continue
+                for j in range(self.IMAGES_PER_EPISODE):
+                    image_url = template % (episode, str(j + 1).zfill(2))
+                    image_name = episode + '_' + str(j + 1)
+                    if self.download_image(image_url, self.base_folder + '/' + image_name) == -1:
+                        stop = True
+                        break
+                if stop:
+                    break
+        except Exception as e:
+            self.print_exception(e)
 
     def download_news(self):
         self.download_template_news(page_prefix=self.PAGE_PREFIX, article_select='#news article', date_select='time',
