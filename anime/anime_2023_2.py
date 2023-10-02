9d653952db1a385d32ffd637b663daf1e686ea2f
tetrix1993 <tetrix1993@hotmail.com>
2023-10-02 18:07:02 +0800

    Added DeadMountDeathPlay Episode Count

--- anime/anime_2023_2.py
+++ anime/anime_2023_2.py
@@ -336,7 +336,7 @@ class DeadMountDeathPlayDownload(Spring2023AnimeDownload, NewsTemplate):
     folder_name = 'dmdp'
 
     PAGE_PREFIX = website
-    FINAL_EPISODE = 12
+    FINAL_EPISODE = 24
     IMAGES_PER_EPISODE = 5
 
     def __init__(self):
