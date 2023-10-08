aea12d47698fdace87b0a7d3bd77cd8489261db4
tetrix1993 <tetrix1993@hotmail.com>
2023-10-08 19:31:27 +0800

    Added SeijonoMaryoku2 Guess

--- anime/anime_2023_4.py
+++ anime/anime_2023_4.py
@@ -2247,6 +2247,8 @@ class SeijonoMaryoku2Download(Fall2023AnimeDownload, NewsTemplate2):
     folder_name = 'seijyonomaryoku2'
 
     PAGE_PREFIX = website
+    FINAL_EPISODE = 12
+    IMAGES_PER_EPISODE = 6
 
     def __init__(self):
         super().__init__()
@@ -2254,6 +2256,7 @@ class SeijonoMaryoku2Download(Fall2023AnimeDownload, NewsTemplate2):
     def run(self):
         self.download_episode_preview()
         self.download_news()
+        self.download_episode_preview_guess()
         self.download_key_visual()
         self.download_character()
         self.download_media()
@@ -2290,6 +2293,37 @@ class SeijonoMaryoku2Download(Fall2023AnimeDownload, NewsTemplate2):
 
     def download_news(self):
         self.download_template_news(self.PAGE_PREFIX)
+
+    def download_episode_preview_guess(self):
+        folder = self.create_custom_directory('guess')
+        template = self.PAGE_PREFIX + 'core_sys/images/contents/%s/block/%s/%s.jpg'
+        is_successful = False
+        for i in range(self.FINAL_EPISODE):
+            episode = str(i + 1).zfill(2)
+            if self.is_image_exists(episode + '_1'):
+                continue
+            is_success = False
+            first = 20 + i
+            second = 31 + 4 * i
+            third = 31 + 6 * i
+            for j in range(self.IMAGES_PER_EPISODE):
+                image_url = template % (str(first).zfill(8), str(second).zfill(8), str(third + j).zfill(8))
+                image_name = episode + '_' + str(j + 1)
+                result = self.download_image(image_url, folder + '/' + image_name)
+                if result == 0:
+                    is_success = True
+                    is_successful = True
+                elif result == -1:
+                    break
+            if is_success:
+                print(self.__class__.__name__ + ' - Guessed successfully!')
+            else:
+                if len(os.listdir(folder)) == 0:
+                    os.rmdir(folder)
+                return
+        if len(os.listdir(folder)) == 0:
+            os.rmdir(folder)
+        return is_successful
 
     def download_key_visual(self):
         folder = self.create_key_visual_directory()
