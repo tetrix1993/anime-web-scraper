1441acffd3012dfb900025027cf9fe812d43c31c
tetrix1993 <tetrix1993@hotmail.com>
2023-12-28 22:17:02 +0800

    Added GekaiElise Episode Preview

--- anime/anime_2024_1.py
+++ anime/anime_2024_1.py
@@ -513,18 +513,42 @@ class GekaiEliseDownload(Winter2024AnimeDownload, NewsTemplate):
     folder_name = 'gekaielise'
 
     PAGE_PREFIX = website
+    FINAL_EPISODE = 12
+    IMAGES_PER_EPISODE = 6
 
     def __init__(self):
         super().__init__()
 
     def run(self):
         self.download_episode_preview()
+        self.download_episode_preview_external()
         self.download_news()
         self.download_key_visual()
         self.download_character()
 
     def download_episode_preview(self):
-        self.has_website_updated(self.PAGE_PREFIX, 'index')
+        try:
+            template = self.PAGE_PREFIX + 'images/story/%s/p_%s.jpg'
+            stop = False
+            for i in range(self.FINAL_EPISODE):
+                episode = str(i + 1).zfill(2)
+                if self.is_image_exists(episode + '_' + str(self.IMAGES_PER_EPISODE)):
+                    continue
+                for j in range(self.IMAGES_PER_EPISODE):
+                    image_url = template % (str(i + 1).zfill(3), str(j + 1).zfill(3))
+                    image_name = episode + '_' + str(j + 1)
+                    if self.download_image(image_url, self.base_folder + '/' + image_name) == -1:
+                        stop = True
+                        break
+                if stop:
+                    break
+        except Exception as e:
+            self.print_exception(e)
+
+    def download_episode_preview_external(self):
+        keywords = ['外科医エリーゼ']
+        AniverseMagazineScanner(keywords, self.base_folder, last_episode=self.FINAL_EPISODE,
+                                end_date='20231223', download_id=self.download_id).run()
 
     def download_news(self):
         try:
