cf1f8ff890eefb6b429bf86a5544dca8cb7be0bd
tetrix1993 <tetrix1993@hotmail.com>
2023-12-29 20:15:48 +0800

    Added DungeonMeshi Episode Preview

--- anime/anime_2024_1.py
+++ anime/anime_2024_1.py
@@ -490,18 +490,42 @@ class DungeonMeshiDownload(Winter2024AnimeDownload, NewsTemplate):
     folder_name = 'dungeon-meshi'
 
     PAGE_PREFIX = website
+    FINAL_EPISODE = 24
+    IMAGES_PER_EPISODE = 8
 
     def __init__(self):
         super().__init__()
 
     def run(self):
         self.download_episode_preview()
+        self.download_episode_preview_external()
         self.download_news()
         self.download_key_visual()
         self.download_character()
 
     def download_episode_preview(self):
-        self.has_website_updated(self.PAGE_PREFIX, 'index')
+        try:
+            template = self.PAGE_PREFIX + 'assets/story/%s/%s.webp'
+            stop = False
+            for i in range(self.FINAL_EPISODE):
+                episode = str(i + 1).zfill(2)
+                if self.is_image_exists(episode + '_' + str(self.IMAGES_PER_EPISODE)):
+                    continue
+                for j in range(self.IMAGES_PER_EPISODE):
+                    image_url = template % (str(i + 1), str(j + 1))
+                    image_name = episode + '_' + str(j + 1)
+                    if self.download_image(image_url, self.base_folder + '/' + image_name, to_jpg=True) == -1:
+                        stop = True
+                        break
+                if stop:
+                    break
+        except Exception as e:
+            self.print_exception(e)
+
+    def download_episode_preview_external(self):
+        keywords = ['ダンジョン飯']
+        AniverseMagazineScanner(keywords, self.base_folder, last_episode=self.FINAL_EPISODE,
+                                end_date='20231229', download_id=self.download_id).run()
 
     def download_news(self):
         self.download_template_news(page_prefix=self.PAGE_PREFIX, article_select='.content-entry',
