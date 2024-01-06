f2890fd0af27c1ef6210fb3bc07e7a83adef457b
tetrix1993 <tetrix1993@hotmail.com>
2024-01-06 22:23:46 +0800

    Added YubisakitoRenren Blu-ray

--- anime/anime_2024_1.py
+++ anime/anime_2024_1.py
@@ -2693,6 +2693,7 @@ class YubisakitoRenrenDownload(Winter2024AnimeDownload, NewsTemplate):
         self.download_episode_preview_guess(print_invalid=False, download_valid=True)
         self.download_key_visual()
         self.download_character()
+        self.download_media()
 
     def download_episode_preview(self):
         self.has_website_updated(self.PAGE_PREFIX, 'index')
@@ -2767,3 +2768,19 @@ class YubisakitoRenrenDownload(Winter2024AnimeDownload, NewsTemplate):
         folder = self.create_character_directory()
         template = self.PAGE_PREFIX + 'wp/wp-content/themes/yubisaki_teaser/images/chara-pic%s.png'
         self.download_by_template(folder, template, 1, 1)
+
+    def download_media(self):
+        folder = self.create_media_directory()
+        try:
+            soup = self.get_soup(self.PAGE_PREFIX + 'bluray/')
+            images = soup.select('.section-contents img[src*="/images/"]')
+            self.image_list = []
+            for image in images:
+                image_url = image['src']
+                if 'bluray-pic-sample' in image_url or 'privilege-pic-sample' in image_url:
+                    continue
+                image_name = self.generate_image_name_from_url(image_url, 'images')
+                self.add_to_image_list(image_name, image_url)
+            self.download_image_list(folder)
+        except Exception as e:
+            self.print_exception(e, 'Key Visual')
