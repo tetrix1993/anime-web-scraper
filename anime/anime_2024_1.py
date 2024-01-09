d6a8cdf5fbd35df01fe7fc11d2e4d42acdf8fb61
tetrix1993 <tetrix1993@hotmail.com>
2024-01-09 17:21:19 +0800

    Added MajotoYajuu Episode Preview

--- anime/anime_2024_1.py
+++ anime/anime_2024_1.py
@@ -1757,6 +1757,8 @@ class MajotoYajuuDownload(Winter2024AnimeDownload, NewsTemplate):
     folder_name = 'majotoyajuu'
 
     PAGE_PREFIX = website
+    FINAL_EPISODE = 12
+    IMAGES_PER_EPISODE = 6
 
     def __init__(self):
         super().__init__()
@@ -1767,7 +1769,23 @@ class MajotoYajuuDownload(Winter2024AnimeDownload, NewsTemplate):
         self.download_key_visual()
 
     def download_episode_preview(self):
-        self.has_website_updated(self.PAGE_PREFIX, 'index')
+        try:
+            template = self.PAGE_PREFIX + 'story/img/story%s/%s.jpg'
+            stop = False
+            for i in range(self.FINAL_EPISODE):
+                episode = str(i + 1).zfill(2)
+                if self.is_image_exists(episode + '_' + str(self.IMAGES_PER_EPISODE)):
+                    continue
+                for j in range(self.IMAGES_PER_EPISODE):
+                    image_name = episode + '_' + str(j + 1)
+                    image_url = template % (episode, str(j + 1).zfill(2))
+                    if self.download_image(image_url, self.base_folder + '/' + image_name) == -1:
+                        stop = True
+                        break
+                if stop:
+                    break
+        except Exception as e:
+            self.print_exception(e)
 
     def download_news(self):
         news_url = self.PAGE_PREFIX + 'news/'
