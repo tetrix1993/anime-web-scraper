ce6ce9bc314d078f64a17d4304b0d3a234f7fa13
tetrix1993 <tetrix1993@hotmail.com>
2024-01-13 22:04:41 +0800

    Updated SaijakuTamer Episode Preview

--- anime/anime_2024_1.py
+++ anime/anime_2024_1.py
@@ -2583,7 +2583,7 @@ class SaijakuTamerDownload(Winter2024AnimeDownload, NewsTemplate):
             stop = False
             for i in range(self.FINAL_EPISODE):
                 episode = str(i + 1).zfill(2)
-                if self.is_image_exists(episode + '_' + str(self.IMAGES_PER_EPISODE)):
+                if self.is_image_exists(episode + '_1'):
                     continue
                 for j in range(self.IMAGES_PER_EPISODE):
                     image_name = episode + '_' + str(j + 1)
