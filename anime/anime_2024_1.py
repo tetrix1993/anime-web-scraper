36cc5ee8c33f2de662c9865c0661cd7c9f84108d
tetrix1993 <tetrix1993@hotmail.com>
2024-01-17 09:22:07 +0800

    Added Kanaten Guess

--- anime/anime_2024_1.py
+++ anime/anime_2024_1.py
@@ -2314,6 +2314,7 @@ class KanatenDownload(Winter2024AnimeDownload, NewsTemplate2):
 
     PAGE_PREFIX = website
     FINAL_EPISODE = 12
+    IMAGES_PER_EPISODE = 6
 
     def __init__(self):
         super().__init__()
@@ -2322,6 +2323,7 @@ class KanatenDownload(Winter2024AnimeDownload, NewsTemplate2):
         self.download_episode_preview()
         self.download_news()
         self.download_episode_preview_external()
+        self.download_episode_preview_guess()
         self.download_key_visual()
         self.download_character()
         self.download_media()
@@ -2360,6 +2362,39 @@ class KanatenDownload(Winter2024AnimeDownload, NewsTemplate2):
         keywords = ['愚かな天使は悪魔と踊る']
         AniverseMagazineScanner(keywords, self.base_folder, last_episode=self.FINAL_EPISODE,
                                 end_date='20240104', download_id=self.download_id).run()
+
+    def download_episode_preview_guess(self):
+        folder = self.create_custom_directory('guess')
+        template = self.PAGE_PREFIX + 'core_sys/images/contents/%s/block/%s/%s.jpg'
+        is_successful = False
+        for i in range(self.FINAL_EPISODE):
+            episode = str(i + 1).zfill(2)
+            if self.is_image_exists(episode + '_1'):
+                continue
+            is_success = False
+            first = 21 + i
+            second = 43 + 4 * i
+            third = 49 + 6 * i
+            for j in range(self.IMAGES_PER_EPISODE):
+                image_url = template % (str(first).zfill(8), str(second).zfill(8), str(third + j).zfill(8))
+                image_name = episode + '_' + str(j + 1)
+                if not self.is_content_length_in_range(image_url, more_than_amount=13000):
+                    break
+                result = self.download_image(image_url, folder + '/' + image_name)
+                if result == 0:
+                    is_success = True
+                    is_successful = True
+                elif result == -1:
+                    break
+            if is_success:
+                print(self.__class__.__name__ + ' - Guessed successfully!')
+            else:
+                if len(os.listdir(folder)) == 0:
+                    os.rmdir(folder)
+                return
+        if len(os.listdir(folder)) == 0:
+            os.rmdir(folder)
+        return is_successful
 
     def download_key_visual(self):
         folder = self.create_key_visual_directory()
