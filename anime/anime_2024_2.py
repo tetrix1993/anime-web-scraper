4d64477bb118f0cf7cc20ee2505eab980c6eec99
tetrix1993 <tetrix1993@hotmail.com>
2024-03-22 12:37:52 +0800

    Added SeiyuRadio Episode Preview

--- anime/anime_2024_2.py
+++ anime/anime_2024_2.py
@@ -923,6 +923,8 @@ class SeiyuRadioDownload(Spring2024AnimeDownload, NewsTemplate):
     folder_name = 'seiyuradio'
 
     PAGE_PREFIX = website
+    FINAL_EPISODE = 12
+    IMAGES_PER_EPISODE = 5
 
     def __init__(self):
         super().__init__()
@@ -934,7 +936,23 @@ class SeiyuRadioDownload(Spring2024AnimeDownload, NewsTemplate):
         self.download_character()
 
     def download_episode_preview(self):
-        self.has_website_updated(self.PAGE_PREFIX, 'index')
+        try:
+            template = self.PAGE_PREFIX + 'dist/img/story/ep%s/img%s.webp'
+            stop = False
+            for i in range(self.FINAL_EPISODE):
+                episode = str(i + 1).zfill(2)
+                if self.is_image_exists(episode + '_' + str(self.IMAGES_PER_EPISODE)):
+                    continue
+                for j in range(self.IMAGES_PER_EPISODE):
+                    image_url = template % (str(i + 1), str(j + 1).zfill(2))
+                    image_name = episode + '_' + str(j + 1)
+                    if self.download_image(image_url, self.base_folder + '/' + image_name, to_jpg=True) == -1:
+                        stop = True
+                        break
+                if stop:
+                    break
+        except Exception as e:
+            self.print_exception(e)
 
     def download_news(self):
         news_url = self.PAGE_PREFIX + 'news/'
