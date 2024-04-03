660fd26f251f76052f9a58cf8903c1967ff8bfd6
tetrix1993 <tetrix1993@hotmail.com>
2024-04-03 21:30:55 +0800

    Added UnnamedMemory Episode Preview

--- anime/anime_2024_2.py
+++ anime/anime_2024_2.py
@@ -1680,6 +1680,8 @@ class UnnamedMemoryDownload(Spring2024AnimeDownload, NewsTemplate):
     folder_name = 'unnamedmemory'
 
     PAGE_PREFIX = website
+    FINAL_EPISODE = 12
+    IMAGES_PER_EPISODE = 6
 
     def __init__(self):
         super().__init__()
@@ -1691,7 +1693,23 @@ class UnnamedMemoryDownload(Spring2024AnimeDownload, NewsTemplate):
         self.download_character()
 
     def download_episode_preview(self):
-        self.has_website_updated(self.PAGE_PREFIX, 'index')
+        try:
+            template = self.PAGE_PREFIX + 'assets/story/%s/%s.webp'
+            stop = False
+            for i in range(self.FINAL_EPISODE):
+                episode = str(i + 1).zfill(2)
+                if self.is_image_exists(episode + '_' + str(self.IMAGES_PER_EPISODE)):
+                    continue
+                for j in range(self.IMAGES_PER_EPISODE):
+                    image_url = template % (str(i + 1), str(j + 1))
+                    image_name = episode + '_' + str(j + 1)
+                    if self.download_image(image_url, self.base_folder + '/' + image_name, to_jpg=True) == -1:
+                        stop = True
+                        break
+                if stop:
+                    break
+        except Exception as e:
+            self.print_exception(e)
 
     def download_news(self):
         self.download_template_news(page_prefix=self.PAGE_PREFIX, article_select='.content-entry',
