4ee901beffc4370e8d574e2cfb9136e72d42cb4e
tetrix1993 <tetrix1993@hotmail.com>
2024-04-06 08:16:01 +0800

    Added JisanBasan Blu-ray

--- anime/anime_2024_2.py
+++ anime/anime_2024_2.py
@@ -367,6 +367,7 @@ class JisanBasanDownload(Spring2024AnimeDownload, NewsTemplate):
         self.download_news()
         soup = self.download_key_visual()
         self.download_character(soup)
+        self.download_media()
 
     def download_episode_preview(self):
         try:
@@ -423,6 +424,22 @@ class JisanBasanDownload(Spring2024AnimeDownload, NewsTemplate):
             self.download_image_list(folder)
         except Exception as e:
             self.print_exception(e, 'Character')
+
+    def download_media(self):
+        folder = self.create_media_directory()
+        try:
+            soup = self.get_soup(self.PAGE_PREFIX + 'bluray.html')
+            images = soup.select('section img[src]')
+            self.image_list = []
+            for image in images:
+                image_url = self.PAGE_PREFIX + image['src'].replace('./', '')
+                image_name = self.extract_image_name_from_url(image_url)
+                if image_name == 'np':
+                    continue
+                self.add_to_image_list(image_name, image_url)
+            self.download_image_list(folder)
+        except Exception as e:
+            self.print_exception(e, 'Blu-ray')
 
 
 # Kaijuu 8-gou
