44f062f07f9b12d58020b51b8111c870d4d3f43c
tetrix1993 <tetrix1993@hotmail.com>
2024-04-11 21:32:49 +0800

    Added Kaiju8 Episode Preview

--- anime/anime_2024_2.py
+++ anime/anime_2024_2.py
@@ -487,6 +487,8 @@ class Kaiju8Download(Spring2024AnimeDownload, NewsTemplate):
     folder_name = 'kaiju8'
 
     PAGE_PREFIX = website
+    FINAL_EPISODE = 12
+    IMAGES_PER_EPISODE = 6
 
     def __init__(self):
         super().__init__()
@@ -498,7 +500,23 @@ class Kaiju8Download(Spring2024AnimeDownload, NewsTemplate):
         self.download_character()
 
     def download_episode_preview(self):
-        self.has_website_updated(self.PAGE_PREFIX, 'index')
+        try:
+            template = self.PAGE_PREFIX + 'episodes/img/%s/%s.jpg'
+            stop = False
+            for i in range(self.FINAL_EPISODE):
+                episode = str(i + 1).zfill(2)
+                if self.is_image_exists(episode + '_' + str(self.IMAGES_PER_EPISODE)):
+                    continue
+                for j in range(self.IMAGES_PER_EPISODE):
+                    image_url = template % (str(i + 1), str(j + 1))
+                    image_name = episode + '_' + str(j + 1)
+                    if self.download_image(image_url, self.base_folder + '/' + image_name) == -1:
+                        stop = True
+                        break
+                if stop:
+                    break
+        except Exception as e:
+            self.print_exception(e)
 
     def download_news(self):
         news_url = self.PAGE_PREFIX + 'news/'
