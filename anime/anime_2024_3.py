47a10e30995328990dabef42156382dcb5c314a4
tetrix1993 <tetrix1993@hotmail.com>
2024-06-05 21:18:58 +0800

    Added Nazeboku Episode Preview

--- anime/anime_2024_3.py
+++ anime/anime_2024_3.py
@@ -1279,6 +1279,8 @@ class NazebokuDownload(Summer2024AnimeDownload, NewsTemplate):
     folder_name = 'nazeboku'
 
     PAGE_PREFIX = website
+    FINAL_EPISODE = 12
+    IMAGES_PER_EPISODE = 5
 
     def __init__(self):
         super().__init__()
@@ -1290,7 +1292,23 @@ class NazebokuDownload(Summer2024AnimeDownload, NewsTemplate):
         self.download_character()
 
     def download_episode_preview(self):
-        self.has_website_updated(self.PAGE_PREFIX, 'index')
+        try:
+            template = self.PAGE_PREFIX + 'dist/img/story/ep%s/img%s.jpg'
+            stop = False
+            for i in range(self.FINAL_EPISODE):
+                episode = str(i + 1).zfill(2)
+                if self.is_image_exists(episode + '_' + str(self.IMAGES_PER_EPISODE)):
+                    continue
+                for j in range(self.IMAGES_PER_EPISODE):
+                    image_url = template % (str(i + 1), str(j + 1).zfill(2))
+                    image_name = episode + '_' + str(j + 1).zfill(2)
+                    if self.download_image(image_url, self.base_folder + '/' + image_name) == -1:
+                        stop = True
+                        break
+                if stop:
+                    break
+        except Exception as e:
+            self.print_exception(e)
 
     def download_news(self):
         self.download_template_news(page_prefix=self.PAGE_PREFIX, article_select='.news_item',
