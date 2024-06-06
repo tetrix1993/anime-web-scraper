a4f6ec7ab3a5e489c98507259285163ad469749b
tetrix1993 <tetrix1993@hotmail.com>
2024-06-06 19:40:02 +0800

    Updated Maougun Episode Preview

--- anime/anime_2024_3.py
+++ anime/anime_2024_3.py
@@ -1021,6 +1021,8 @@ class MaougunDownload(Summer2024AnimeDownload, NewsTemplate):
     folder_name = 'maougun'
 
     PAGE_PREFIX = website
+    FINAL_EPISODE = 12
+    IMAGES_PER_EPISODE = 6
 
     def __init__(self):
         super().__init__()
@@ -1032,7 +1034,23 @@ class MaougunDownload(Summer2024AnimeDownload, NewsTemplate):
         self.download_character(soup)
 
     def download_episode_preview(self):
-        self.has_website_updated(self.PAGE_PREFIX, 'index')
+        try:
+            template = self.PAGE_PREFIX + 'dist/img/top/story/ep%s/img%s.jpg'
+            stop = False
+            for i in range(self.FINAL_EPISODE):
+                episode = str(i + 1).zfill(2)
+                if self.is_image_exists(episode + '_' + str(self.IMAGES_PER_EPISODE)):
+                    continue
+                for j in range(self.IMAGES_PER_EPISODE):
+                    image_url = template % (str(i + 1), str(j + 1))
+                    image_name = episode + '_' + str(j + 1)
+                    if self.download_image(image_url, self.base_folder + '/' + image_name) == -1:
+                        stop = True
+                        break
+                if stop:
+                    break
+        except Exception as e:
+            self.print_exception(e)
 
     def download_news(self):
         self.download_template_news(page_prefix=self.PAGE_PREFIX, article_select='.c-Post__link',
@@ -1301,7 +1319,7 @@ class NazebokuDownload(Summer2024AnimeDownload, NewsTemplate):
                     continue
                 for j in range(self.IMAGES_PER_EPISODE):
                     image_url = template % (str(i + 1), str(j + 1).zfill(2))
-                    image_name = episode + '_' + str(j + 1).zfill(2)
+                    image_name = episode + '_' + str(j + 1)
                     if self.download_image(image_url, self.base_folder + '/' + image_name) == -1:
                         stop = True
                         break
