77a32bd53fb4f66601201f5ae9fd94e42bf11e5f
tetrix1993 <tetrix1993@hotmail.com>
2024-06-07 20:24:02 +0800

    Added GimaiSeikatsu Blu-ray

--- anime/anime_2024_3.py
+++ anime/anime_2024_3.py
@@ -405,6 +405,7 @@ class GimaiSeikatsuDownload(Summer2024AnimeDownload, NewsTemplate2):
         self.download_news()
         self.download_key_visual()
         self.download_character()
+        self.download_media()
 
     def download_episode_preview(self):
         self.has_website_updated(self.PAGE_PREFIX, 'index')
@@ -453,6 +454,39 @@ class GimaiSeikatsuDownload(Summer2024AnimeDownload, NewsTemplate2):
                     self.download_image_list(folder)
         except Exception as e:
             self.print_exception(e, 'Character')
+        self.create_cache_file(cache_filepath, processed, num_processed)
+
+    def download_media(self):
+        folder = self.create_media_directory()
+        cache_filepath = folder + '/cache'
+        processed, num_processed = self.get_processed_items_from_cache_file(cache_filepath)
+        for page in ['privilege', 'campaign', '01', '02', '03']:
+            try:
+                if page in processed:
+                    continue
+                page_url = self.PAGE_PREFIX + 'bd/'
+                if page != '01':
+                    page_url += page + '.html'
+                soup = self.get_soup(page_url)
+                images = soup.select('.block_inner img[src]')
+                self.image_list = []
+                for image in images:
+                    image_url = self.PAGE_PREFIX + image['src'].replace('../', '').split('?')[0]
+                    if '/bddvd/' in image_url:
+                        image_name = self.generate_image_name_from_url(image_url, 'bddvd')
+                    else:
+                        image_name = self.extract_image_name_from_url(image_url)
+                    if not self.is_content_length_in_range(image_url, more_than_amount=25000):
+                        continue
+                    self.add_to_image_list(image_name, image_url)
+                if page.isnumeric() and page != '01':
+                    if len(self.image_list) > 0:
+                        processed.append(page)
+                    else:
+                        break
+                self.download_image_list(folder)
+            except Exception as e:
+                self.print_exception(e, f'Blu-ray - {page}')
         self.create_cache_file(cache_filepath, processed, num_processed)
 
 
@@ -1810,8 +1844,8 @@ class RoshidereDownload(Summer2024AnimeDownload, NewsTemplate2):
                 self.image_list = []
                 for image in images:
                     image_url = self.PAGE_PREFIX + image['src'].replace('../', '').split('?')[0]
-                    if '/bd/' in image_url:
-                        image_name = self.generate_image_name_from_url(image_url, 'bd')
+                    if '/bddvd/' in image_url:
+                        image_name = self.generate_image_name_from_url(image_url, 'bddvd')
                     else:
                         image_name = self.extract_image_name_from_url(image_url)
                     if not self.is_content_length_in_range(image_url, more_than_amount=32000):
@@ -1835,7 +1869,6 @@ class RoshidereDownload(Summer2024AnimeDownload, NewsTemplate2):
                 if self.is_image_exists(folder, image_name):
                     continue
                 image_url = prefix % (str(img[0]).zfill(8), str(img[1]).zfill(8), str(img[2]).zfill(8))
-                print(image_url)
                 if not self.is_content_length_in_range(image_url, more_than_amount=32000):
                     continue
                 self.add_to_image_list(image_name, image_url)
