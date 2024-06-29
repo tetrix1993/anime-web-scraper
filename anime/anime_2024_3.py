d716b93d082afc4940da994a77ec39e782b10700
tetrix1993 <tetrix1993@hotmail.com>
2024-06-29 11:11:43 +0800

    Added Mobkara Episode Preview

--- anime/anime_2024_3.py
+++ anime/anime_2024_3.py
@@ -1336,7 +1336,25 @@ class MobkaraDownload(Summer2024AnimeDownload, NewsTemplate):
         self.download_character()
 
     def download_episode_preview(self):
-        self.has_website_updated(self.PAGE_PREFIX, 'index')
+        try:
+            soup = self.get_soup(self.PAGE_PREFIX + 'story')
+            blocks = soup.select('.storyBox[id]')
+            for block in blocks:
+                try:
+                    episode = str(int(block['id'].replace('story', ''))).zfill(2)
+                except:
+                    continue
+                images = block.select('.imgList__item img[src]')
+                self.image_list = []
+                for i in range(len(images)):
+                    image_url = images[i]['src']
+                    if image_url.startswith('/'):
+                        image_url = self.PAGE_PREFIX + image_url[1:]
+                    image_name = episode + '_' + str(i + 1)
+                    self.add_to_image_list(image_name, image_url)
+                self.download_image_list(self.base_folder)
+        except Exception as e:
+            self.print_exception(e)
 
     def download_news(self):
         self.download_template_news(page_prefix=self.PAGE_PREFIX, article_select='.newsList',
