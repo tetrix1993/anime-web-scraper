14904996fd4c6d2caf7ad7fb5ce9b1ba4b1a142f
tetrix1993 <tetrix1993@hotmail.com>
2024-07-01 18:56:16 +0800

    Added Oshinoko2 Episode Preview

--- anime/anime_2024_3.py
+++ anime/anime_2024_3.py
@@ -1616,18 +1616,49 @@ class Oshinoko2Download(Summer2024AnimeDownload, NewsTemplate2):
 
     PAGE_PREFIX = website
     IMAGES_PER_EPISODE = 6
+    FINAL_EPISODE = 24
 
     def __init__(self):
         super().__init__()
 
     def run(self):
         self.download_episode_preview()
+        self.download_episode_preview_external()
         self.download_news()
         self.download_key_visual()
         self.download_character()
 
     def download_episode_preview(self):
-        self.has_website_updated(self.PAGE_PREFIX, 'index')
+        try:
+            soup = self.get_soup(self.PAGE_PREFIX + 'story/', decode=True)
+            stories = soup.select('#ContentsListUnit02 a[href]')
+            for story in stories:
+                try:
+                    ep_num = self.convert_kanji_to_number(story.text.replace('第', '').replace('話', ''))
+                    if ep_num is None:
+                        continue
+                    episode = str(ep_num).zfill(2)
+                except:
+                    continue
+                if self.is_image_exists(episode + '_1'):
+                    continue
+                ep_soup = self.get_soup(self.PAGE_PREFIX + story['href'].replace('../', ''))
+                if ep_soup is None:
+                    continue
+                self.image_list = []
+                images = ep_soup.select('ul.tp5 img[src]')
+                for i in range(len(images)):
+                    image_url = self.PAGE_PREFIX + images[i]['src'].split('?')[0].replace('../', '').replace('sn_', '')
+                    image_name = episode + '_' + str(i + 1)
+                    self.add_to_image_list(image_name, image_url)
+                self.download_image_list(self.base_folder)
+        except Exception as e:
+            self.print_exception(e)
+
+    def download_episode_preview_external(self):
+        keywords = ['推しの子']
+        AniverseMagazineScanner(keywords, self.base_folder, last_episode=self.FINAL_EPISODE,
+                                end_date='20240701', download_id=self.download_id).run()
 
     def download_news(self):
         self.download_template_news(self.PAGE_PREFIX)
