719ec8d7c6ccaa63b96066ec036b31fe203e965e
tetrix1993 <tetrix1993@hotmail.com>
2024-07-03 17:04:59 +0800

    Added Konofuka Episode Preview

--- anime/anime_2024_3.py
+++ anime/anime_2024_3.py
@@ -1039,7 +1039,29 @@ class KonofukaDownload(Summer2024AnimeDownload, NewsTemplate):
         self.download_character()
 
     def download_episode_preview(self):
-        self.has_website_updated(self.PAGE_PREFIX, 'index')
+        try:
+            soup = self.get_soup(self.PAGE_PREFIX + 'story')
+            stories = soup.select('.story-episode-item a[href]')
+            for story in stories:
+                try:
+                    episode = str(int(story.select('.story-episode-ttl')[0].text
+                                      .split(' ')[0].replace('No.', ''))).zfill(2)
+                except:
+                    continue
+                if self.is_image_exists(episode + '_1'):
+                    continue
+                ep_soup = self.get_soup(story['href'])
+                if ep_soup is None:
+                    continue
+                images = ep_soup.select('.story-thumb-list img[src]')
+                self.image_list = []
+                for i in range(len(images)):
+                    image_url = images[i]['src']
+                    image_name = episode + '_' + str(i + 1)
+                    self.add_to_image_list(image_name, image_url)
+                self.download_image_list(self.base_folder)
+        except Exception as e:
+            self.print_exception(e)
 
     def download_news(self):
         self.download_template_news(page_prefix=self.PAGE_PREFIX, article_select='.news-box',
