155c07ed3d811b842cbc79f8e8bb9649160f4b05
tetrix1993 <tetrix1993@hotmail.com>
2024-07-05 10:14:20 +0800

    Removed Bokutsuma from main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def run():
 
     downloads = []
     for subclass in Summer2024AnimeDownload.__subclasses__():
-        if subclass.enabled:
+        if subclass.enabled and subclass.__name__ != BokutsumaDownload.__name__:
             downloads.append(subclass())
     downloads += [OokamitoKoushinryouDownload(), YozakurasanDownload(), NierAutomataDownload(), Maohgakuin2Download(),
                   KanteiSkillDownload(), Konosuba3Download(), TenshitsukiDownload(), UnnamedMemoryDownload(),
