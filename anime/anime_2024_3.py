ae445196f694b19a0d53ef5a638fd016c02f0f28
tetrix1993 <tetrix1993@hotmail.com>
2024-07-09 23:32:19 +0800

    Added Konofuka Guess

--- anime/anime_2024_3.py
+++ anime/anime_2024_3.py
@@ -1175,6 +1175,8 @@ class KonofukaDownload(Summer2024AnimeDownload, NewsTemplate):
     folder_name = 'konofuka'
 
     PAGE_PREFIX = website
+    FINAL_EPISODE = 13
+    IMAGES_PER_EPISODE = 6
 
     def __init__(self):
         super().__init__()
@@ -1182,6 +1184,7 @@ class KonofukaDownload(Summer2024AnimeDownload, NewsTemplate):
     def run(self):
         self.download_episode_preview()
         self.download_news()
+        # self.download_episode_preview_guess()
         self.download_key_visual()
         self.download_character()
 
@@ -1209,6 +1212,42 @@ class KonofukaDownload(Summer2024AnimeDownload, NewsTemplate):
                 self.download_image_list(self.base_folder)
         except Exception as e:
             self.print_exception(e)
+
+    def download_episode_preview_guess(self, print_invalid=False, download_valid=False):
+        if self.is_image_exists(str(self.FINAL_EPISODE).zfill(2) + '_1'):
+            return
+
+        folder = self.create_custom_directory('guess')
+        template = self.PAGE_PREFIX + 'pDK2yjkH/wp-content/uploads/%s/%s/%s_このふか%s_%s.jpg'
+        current_date = datetime.now() + timedelta(hours=1)
+        year = current_date.strftime('%Y')
+        month = current_date.strftime('%m')
+        is_successful = False
+        for i in range(self.FINAL_EPISODE):
+            episode = str(i + 1).zfill(2)
+            if self.is_image_exists(episode + '_1') or self.is_image_exists(episode + '_1', folder):
+                continue
+            episode_success = False
+            for j in range(self.IMAGES_PER_EPISODE):
+                prefix = '00_メイン' if j == 0 else str(j).zfill(2)
+                for k in range(350):
+                    image_url = template % (year, month, prefix, episode, str(k).zfill(4))
+                    if self.is_valid_url(image_url, is_image=True):
+                        print('VALID - ' + image_url)
+                        episode_success = True
+                        if download_valid:
+                            self.download_image(image_url, folder + '/' + episode + '_' + str(j + 1))
+                        break
+                    elif print_invalid:
+                        print('INVALID - ' + image_url)
+                if not episode_success:
+                    break
+            if not episode_success:
+                break
+            is_successful = True
+        if is_successful:
+            print(self.__class__.__name__ + ' - Guessed correctly!')
+        return is_successful
 
     def download_news(self):
         self.download_template_news(page_prefix=self.PAGE_PREFIX, article_select='.news-box',
