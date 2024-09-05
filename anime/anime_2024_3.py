1976e9cb5435c7bcf293c4d1e5eb10ffc91b3cc9
tetrix1993 <tetrix1993@hotmail.com>
2024-09-05 16:43:10 +0800

    Disable Konofuka News

--- anime/anime_2024_3.py
+++ anime/anime_2024_3.py
@@ -1240,7 +1240,7 @@ class KonofukaDownload(Summer2024AnimeDownload, NewsTemplate):
 
     def run(self):
         self.download_episode_preview()
-        self.download_news()
+        # self.download_news()
         # self.download_episode_preview_guess()
         self.download_key_visual()
         self.download_character()
