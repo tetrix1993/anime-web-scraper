7ed20313b0653654a3d8fc6bf0db7bdb3b14d61b
tetrix1993 <tetrix1993@hotmail.com>
2024-10-01 17:17:36 +0800

    Added Negagura Episode Preview

--- anime/anime_2024_4.py
+++ anime/anime_2024_4.py
@@ -660,6 +660,8 @@ class NegaguraDownload(Fall2024AnimeDownload):
     folder_name = 'negagura'
 
     PAGE_PREFIX = website
+    FINAL_EPISODE = 12
+    IMAGES_PER_EPISODE = 6
 
     def __init__(self):
         super().__init__()
@@ -670,7 +672,23 @@ class NegaguraDownload(Fall2024AnimeDownload):
         self.download_key_visual()
 
     def download_episode_preview(self):
-        self.has_website_updated(self.PAGE_PREFIX, 'index')
+        try:
+            template = self.PAGE_PREFIX + 'images/story/%s/p_%s.jpg'
+            stop = False
+            for i in range(self.FINAL_EPISODE):
+                episode = str(i + 1).zfill(2)
+                if self.is_image_exists(episode + '_' + str(self.IMAGES_PER_EPISODE)):
+                    continue
+                for j in range(self.IMAGES_PER_EPISODE):
+                    image_url = template % (str(i + 1).zfill(3), str(j + 1).zfill(3))
+                    image_name = episode + '_' + str(j + 1)
+                    if self.download_image(image_url, self.base_folder + '/' + image_name) == -1:
+                        stop = True
+                        break
+                if stop:
+                    break
+        except Exception as e:
+            self.print_exception(e)
 
     def download_news(self):
         try:
