49658226ea2ef370241b269ea372d939b92e72c3
tetrix1993 <tetrix1993@hotmail.com>
2024-10-02 17:12:28 +0800

    Added SayounaraRyuusei Episode Preview

--- anime/anime_2024_4.py
+++ anime/anime_2024_4.py
@@ -1043,6 +1043,8 @@ class SayounaraRyuuseiDownload(Fall2024AnimeDownload, NewsTemplate):
     folder_name = 'sayounararyuusei'
 
     PAGE_PREFIX = website
+    IMAGES_PER_EPISODE = 8
+    FINAL_EPISODE = 12
 
     def run(self):
         self.download_episode_preview()
@@ -1051,7 +1053,23 @@ class SayounaraRyuuseiDownload(Fall2024AnimeDownload, NewsTemplate):
         self.download_character()
 
     def download_episode_preview(self):
-        self.has_website_updated(self.PAGE_PREFIX, 'index')
+        try:
+            template = self.PAGE_PREFIX + 'images/episodes/%s/img_%s.jpg'
+            stop = False
+            for i in range(self.FINAL_EPISODE):
+                episode = str(i + 1).zfill(2)
+                if self.is_image_exists(episode + '_' + str(self.IMAGES_PER_EPISODE)):
+                    continue
+                for j in range(self.IMAGES_PER_EPISODE):
+                    image_url = template % (episode, str(j + 1).zfill(2))
+                    image_name = episode + '_' + str(j + 1)
+                    if self.download_image(image_url, self.base_folder + '/' + image_name) == -1:
+                        stop = True
+                        break
+                if stop:
+                    break
+        except Exception as e:
+            self.print_exception(e)
 
     def download_news(self):
         news_url = self.PAGE_PREFIX + 'news/'
