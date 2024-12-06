98592e1e1cf0b9ecfa77bbfbc19bb152e12b8088
tetrix1993 <tetrix1993@hotmail.com>
2024-12-06 23:14:19 +0800

    Added Medalist Episode Preview

--- anime/anime_2025_1.py
+++ anime/anime_2025_1.py
@@ -502,6 +502,8 @@ class MedalistDownload(Winter2025AnimeDownload, NewsTemplate):
     folder_name = 'medalist'
 
     PAGE_PREFIX = website
+    FINAL_EPISODE = 12
+    IMAGES_PER_EPISODE = 6
 
     def __init__(self):
         super().__init__()
@@ -511,7 +513,20 @@ class MedalistDownload(Winter2025AnimeDownload, NewsTemplate):
         self.download_news()
 
     def download_episode_preview(self):
-        self.has_website_updated(self.PAGE_PREFIX, 'index')
+        template = self.PAGE_PREFIX + 'assets/story/%s/%s.webp'
+        try:
+            for i in range(self.FINAL_EPISODE):
+                episode = str(i + 1).zfill(2)
+                if self.is_image_exists(episode + '_' + str(self.IMAGES_PER_EPISODE)):
+                    continue
+                for j in range(self.IMAGES_PER_EPISODE):
+                    image_url = template % (str(i + 1), str(j + 1))
+                    image_name = episode + '_' + str(j + 1)
+                    result = self.download_image(image_url, self.base_folder + '/' + image_name, to_jpg=True)
+                    if result == -1:
+                        return
+        except Exception as e:
+            self.print_exception(e)
 
     def download_news(self):
         self.download_template_news(page_prefix=self.PAGE_PREFIX, article_select='article',
