1c6ee49d3bd2552eab0bf184eec4379f5ef96f65
tetrix1993 <tetrix1993@hotmail.com>
2024-12-27 10:01:24 +0800

    Added GrisaiaPT Episode Preview

--- anime/anime_2025_1.py
+++ anime/anime_2025_1.py
@@ -274,7 +274,41 @@ class GrisaiaPTDownload(Winter2025AnimeDownload, NewsTemplate):
         self.download_news()
 
     def download_episode_preview(self):
-        self.has_website_updated(self.PAGE_PREFIX, 'index')
+        try:
+            soup = self.get_soup(self.PAGE_PREFIX + 'story/')
+            stories = soup.select('.page_list li')
+            for story in stories:
+                a_tag = story.select('a[href]')
+                if len(a_tag) == 0:
+                    continue
+                if a_tag[0].text.startswith('OVA'):
+                    continue
+                try:
+                    episode = ''
+                    for s in a_tag[0].text:
+                        if s.isnumeric():
+                            episode += s
+                    if len(episode) == 0:
+                        continue
+                    episode = str(int(episode)).zfill(2)
+                except:
+                    continue
+                if self.is_image_exists(episode + '_1'):
+                    continue
+                if story.has_attr('class') and 'active' in story['class']:
+                    ep_soup = soup
+                else:
+                    ep_soup = self.get_soup(a_tag[0]['href'])
+                if ep_soup is None:
+                    continue
+                images = ep_soup.select('.slide img[src]')
+                for i in range(len(images)):
+                    image_url = images[i]['src']
+                    image_name = episode + '_' + str(i + 1)
+                    self.add_to_image_list(image_name, image_url)
+                self.download_image_list(self.base_folder)
+        except Exception as e:
+            self.print_exception(e)
 
     def download_news(self):
         self.download_template_news(page_prefix=self.PAGE_PREFIX, article_select='.news_area ul.list>li',
