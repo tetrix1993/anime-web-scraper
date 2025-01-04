7ca3fe8f5ec29a537ceb1614c5a784f43cfda22f
tetrix1993 <tetrix1993@hotmail.com>
2025-01-04 20:38:03 +0800

    Added Shiunji

--- anime/anime_2025_2.py
+++ anime/anime_2025_2.py
@@ -121,6 +121,60 @@ class OssanKenseiDownload(Spring2025AnimeDownload, NewsTemplate2):
         self.download_template_news(page_prefix=self.PAGE_PREFIX)
 
 
+# Shiunji-ke no Kodomotachi
+class ShinunjiDownload(Spring2025AnimeDownload):
+    title = 'Shiunji-ke no Kodomotachi'
+    keywords = [title, 'The Shiunji Family Children']
+    website = 'https://shiunjifamily.com/'
+    twitter = 'shiunji_anime'
+    hashtags = ['紫雲寺家の子供たち', 'shiunjifamily']
+    folder_name = 'shiunji'
+
+    PAGE_PREFIX = website
+
+    def __init__(self):
+        super().__init__()
+
+    def run(self):
+        self.download_episode_preview()
+        self.download_news()
+
+    def download_episode_preview(self):
+        self.has_website_updated(self.PAGE_PREFIX, 'index')
+
+    def download_news(self):
+        try:
+            results = []
+            news_obj = self.get_last_news_log_object()
+            news_prefix = self.PAGE_PREFIX + 'news/'
+            json_obj = self.get_json(news_prefix + 'newslist.json')
+            for item in json_obj:
+                if 'date' in item and 'uniqueId' in item and 'title' in item:
+                    try:
+                        date = item['date']
+                    except:
+                        continue
+                    title = item['title']
+                    unique_id = item['uniqueId']
+                    if len(unique_id) == 0 and 'directLinkUrl' in item and len(item['directLinkUrl']) > 1:
+                        url = self.PAGE_PREFIX + item['directLinkUrl'][1:]
+                    else:
+                        url = news_prefix + '?id=' + item['uniqueId']
+                    if news_obj is not None and (news_obj['id'] == url or news_obj['title'] == title
+                                                 or date < news_obj['date']):
+                        break
+                    results.append(self.create_news_log_object(date, title, url))
+            success_count = 0
+            for result in reversed(results):
+                process_result = self.create_news_log_from_news_log_object(result)
+                if process_result == 0:
+                    success_count += 1
+            if len(results) > 0:
+                self.create_news_log_cache(success_count, results[0])
+        except Exception as e:
+            self.print_exception(e, 'News')
+
+
 # Slime Taoshite 300-nen, Shiranai Uchi ni Level Max ni Nattemashita: Sono Ni
 class Slime3002Download(Spring2025AnimeDownload, NewsTemplate):
     title = "Slime Taoshite 300-nen, Shiranai Uchi ni Level Max ni Nattemashita: Sono Ni"
