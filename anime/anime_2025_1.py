6ef6cde4d1f9d3427858674ec1461a24178fe34c
tetrix1993 <tetrix1993@hotmail.com>
2025-01-08 17:09:42 +0800

    Added IsekaiRed Episode Preview

--- anime/anime_2025_1.py
+++ anime/anime_2025_1.py
@@ -963,7 +963,7 @@ class MedalistDownload(Winter2025AnimeDownload, NewsTemplate):
     folder_name = 'medalist'
 
     PAGE_PREFIX = website
-    FINAL_EPISODE = 12
+    FINAL_EPISODE = 13
     IMAGES_PER_EPISODE = 6
 
     def __init__(self):
@@ -1292,6 +1292,8 @@ class IsekaiRedDownload(Winter2025AnimeDownload, NewsTemplate):
     folder_name = 'isekaired'
 
     PAGE_PREFIX = website
+    FINAL_EPISODE = 12
+    IMAGES_PER_EPISODE = 8
 
     def __init__(self):
         super().__init__()
@@ -1301,7 +1303,23 @@ class IsekaiRedDownload(Winter2025AnimeDownload, NewsTemplate):
         self.download_news()
 
     def download_episode_preview(self):
-        self.has_website_updated(self.PAGE_PREFIX, 'index')
+        try:
+            template = self.PAGE_PREFIX + 'assets/story/%s/%s.webp'
+            stop = False
+            for i in range(self.FINAL_EPISODE):
+                episode = str(i + 1).zfill(2)
+                if self.is_image_exists(episode + '_' + str(self.IMAGES_PER_EPISODE)):
+                    continue
+                for j in range(self.IMAGES_PER_EPISODE):
+                    image_url = template % (str(i + 1), str(j + 1))
+                    image_name = episode + '_' + str(j + 1)
+                    if self.download_image(image_url, self.base_folder + '/' + image_name, to_jpg=True) == -1:
+                        stop = True
+                        break
+                if stop:
+                    break
+        except Exception as e:
+            self.print_exception(e)
 
     def download_news(self):
         self.download_template_news(page_prefix=self.PAGE_PREFIX, article_select='article', title_select='.entry-title',
