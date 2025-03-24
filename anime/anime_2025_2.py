bfb9798caf3a0cf3ec76ad2904e0ff81c57b0b53
tetrix1993 <tetrix1993@hotmail.com>
2025-03-24 18:58:47 +0800

    Added Shiunji Episode Preview

--- anime/anime_2025_2.py
+++ anime/anime_2025_2.py
@@ -572,6 +572,8 @@ class ShinunjiDownload(Spring2025AnimeDownload):
     folder_name = 'shiunji'
 
     PAGE_PREFIX = website
+    FINAL_EPISODE = 12
+    IMAGES_PER_EPISODE = 8
 
     def __init__(self):
         super().__init__()
@@ -581,7 +583,23 @@ class ShinunjiDownload(Spring2025AnimeDownload):
         self.download_news()
 
     def download_episode_preview(self):
-        self.has_website_updated(self.PAGE_PREFIX, 'index')
+        template = self.PAGE_PREFIX + 'story/img/ep%s/%s_%s.jpg'
+        try:
+            stop = False
+            for i in range(self.FINAL_EPISODE):
+                episode = str(i + 1).zfill(2)
+                if self.is_image_exists(episode + '_' + str(self.IMAGES_PER_EPISODE)):
+                    continue
+                for j in range(self.IMAGES_PER_EPISODE):
+                    image_url = template % (episode, episode, str(j + 1))
+                    image_name = episode + '_' + str(j + 1)
+                    if self.download_image(image_url, self.base_folder + '/' + image_name, to_jpg=True) == -1:
+                        stop = True
+                        break
+                if stop:
+                    break
+        except Exception as e:
+            self.print_exception(e)
 
     def download_news(self):
         try:
