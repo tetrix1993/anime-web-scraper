1aaa5813875598cc4df13966e48a483f6aed5515
tetrix1993 <tetrix1993@hotmail.com>
2025-03-27 18:39:12 +0800

    Added YamiHealer Episode Preview

--- anime/anime_2025_2.py
+++ anime/anime_2025_2.py
@@ -260,6 +260,8 @@ class YamiHealerDownload(Spring2025AnimeDownload, NewsTemplate):
     folder_name = 'yamihealer'
 
     PAGE_PREFIX = website
+    FINAL_EPISODE = 12
+    IMAGES_PER_EPISODE = 8
 
     def __init__(self):
         super().__init__()
@@ -269,7 +271,23 @@ class YamiHealerDownload(Spring2025AnimeDownload, NewsTemplate):
         self.download_news()
 
     def download_episode_preview(self):
-        self.has_website_updated(self.PAGE_PREFIX, 'index')
+        try:
+            template = self.PAGE_PREFIX + 'assets/img/episode/%s/episode%s_%s.jpg'
+            stop = False
+            for i in range(self.FINAL_EPISODE):
+                episode = str(i + 1).zfill(2)
+                if self.is_image_exists(episode + '_' + str(self.IMAGES_PER_EPISODE)):
+                    continue
+                for j in range(self.IMAGES_PER_EPISODE):
+                    image_url = template % (episode, episode, str(j + 1).zfill(2))
+                    image_name = episode + '_' + str(j + 1)
+                    if self.download_image(image_url, self.base_folder + '/' + image_name, to_jpg=True) == -1:
+                        stop = True
+                        break
+                if stop:
+                    break
+        except Exception as e:
+            self.print_exception(e)
 
     def download_news(self):
         self.download_template_news(page_prefix=self.PAGE_PREFIX, article_select='.p-news__item',
