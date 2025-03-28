6d527e85ef848ccd0ad36ecc132bdafe2982fe61
tetrix1993 <tetrix1993@hotmail.com>
2025-03-28 09:38:58 +0800

    Added Ballpark Episode Preview

--- anime/anime_2025_2.py
+++ anime/anime_2025_2.py
@@ -83,6 +83,8 @@ class BallparkDownload(Spring2025AnimeDownload, NewsTemplate):
     folder_name = 'ballpark'
 
     PAGE_PREFIX = website
+    FINAL_EPISODE = 12
+    IMAGES_PER_EPISODE = 6
 
     def __init__(self):
         super().__init__()
@@ -92,7 +94,25 @@ class BallparkDownload(Spring2025AnimeDownload, NewsTemplate):
         self.download_news()
 
     def download_episode_preview(self):
-        self.has_website_updated(self.PAGE_PREFIX, 'index')
+        template = self.PAGE_PREFIX + 'dist/img/story/ep%s/img%s.jpg'
+        try:
+            stop = False
+            for i in range(self.FINAL_EPISODE):
+                episode = str(i + 1).zfill(2)
+                if self.is_image_exists(episode + '_' + str(self.IMAGES_PER_EPISODE)):
+                    continue
+                for j in range(self.IMAGES_PER_EPISODE):
+                    image_url = template % (str(i + 1), str(j + 1))
+                    image_name = episode + '_' + str(j + 1)
+                    prev_name = str(i).zfill(2) + '_' + str(j + 1)
+                    if (i > 0 and j == 0 and self.is_content_length_same_as_existing(image_url, prev_name))\
+                            or self.download_image(image_url, self.base_folder + '/' + image_name, to_jpg=True) == -1:
+                        stop = True
+                        break
+                if stop:
+                    break
+        except Exception as e:
+            self.print_exception(e)
 
     def download_news(self):
         self.download_template_news(page_prefix=self.PAGE_PREFIX, article_select='.c-Post__listItem',
