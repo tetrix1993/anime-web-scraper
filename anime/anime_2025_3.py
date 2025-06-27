60632504c2b633bb3d6a73c096916affc7805741
tetrix1993 <tetrix1993@hotmail.com>
2025-06-27 21:00:15 +0800

    Added Busagachi Episode Preview

--- anime/anime_2025_3.py
+++ anime/anime_2025_3.py
@@ -103,7 +103,34 @@ class BusagachiDownload(Summer2025AnimeDownload, NewsTemplate):
         self.download_news()
 
     def download_episode_preview(self):
-        self.has_website_updated(self.PAGE_PREFIX, 'index')
+        try:
+            soup = self.get_soup(self.PAGE_PREFIX + 'episode/')
+            stories = soup.select('.episode__number-item a[href]')
+            for story in stories:
+                if not story['href'].startswith('#'):
+                    continue
+                try:
+                    episode = ''
+                    ep_num = story.text
+                    for a in ep_num:
+                        if a.isnumeric():
+                            episode += a
+                    if len(episode) == 0:
+                        continue
+                    episode = str(int(episode)).zfill(2)
+                except:
+                    continue
+                if self.is_image_exists(episode + '_1'):
+                    continue
+                images = soup.select(story['href'] + ' .episode__contents-sliderItem-wrap img[src]')
+                self.image_list = []
+                for i in range(len(images)):
+                    image_url = images[i]['src'].split('?')[0]
+                    image_name = episode + '_' + str(i + 1)
+                    self.add_to_image_list(image_name, image_url, to_jpg=True)
+                self.download_image_list(self.base_folder)
+        except Exception as e:
+            self.print_exception(e)
 
     def download_news(self):
         self.download_template_news(page_prefix=self.PAGE_PREFIX, article_select='.archive__list',
