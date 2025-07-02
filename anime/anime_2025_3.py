189e82ffcdfe5ace32305731d5f9a8be031d6d00
tetrix1993 <tetrix1993@hotmail.com>
2025-07-02 17:30:02 +0800

    Added Kisekoi2 Episode Preview

--- anime/anime_2025_3.py
+++ anime/anime_2025_3.py
@@ -1001,7 +1001,40 @@ class Kisekoi2Download(Summer2025AnimeDownload, NewsTemplate):
         self.download_news()
 
     def download_episode_preview(self):
-        self.has_website_updated(self.PAGE_PREFIX, 'index')
+        prefix = self.PAGE_PREFIX + 'story/'
+        try:
+            soup = self.get_soup(prefix)
+            stories = soup.select('.p-story__nav-list-item')
+            for story in stories:
+                try:
+                    episode = ''
+                    ep_num = story.select('.p-in-text')[0].text
+                    for a in ep_num:
+                        if a.isnumeric():
+                            episode += a
+                    if len(episode) == 0:
+                        continue
+                    episode = str(int(episode)).zfill(2)
+                except:
+                    continue
+                if 'is-current' in story['class']:
+                    ep_soup = soup
+                else:
+                    try:
+                        ep_soup = self.get_soup(prefix + story.select('a[href]')[0]['href'].replace('./', ''))
+                    except:
+                        continue
+                if ep_soup is None:
+                    continue
+                self.image_list = []
+                images = ep_soup.select('.p-story__main-image-list-item img[src]')
+                for i in range(len(images)):
+                    image_url = prefix + images[i]['src']
+                    image_name = episode + '_' + str(i + 1)
+                    self.add_to_image_list(image_name, image_url, to_jpg=True)
+                self.download_image_list(self.base_folder)
+        except Exception as e:
+            self.print_exception(e)
 
     def download_news(self):
         self.download_template_news(page_prefix=self.PAGE_PREFIX, article_select='.l-news__list-item',
