c4dcfe5f7da134649708c0972a7081212911ee37
tetrix1993 <tetrix1993@hotmail.com>
2025-07-09 16:10:57 +0800

    Updated MizuZokusei Episode Preview

--- anime/anime_2025_3.py
+++ anime/anime_2025_3.py
@@ -945,6 +945,8 @@ class MizuZokuseiDownload(Summer2025AnimeDownload, NewsTemplate):
     folder_name = 'mizuzokusei'
 
     PAGE_PREFIX = website
+    FINAL_EPISODE = 12
+    IMAGES_PER_EPISODE = 5
 
     def __init__(self):
         super().__init__()
@@ -954,7 +956,23 @@ class MizuZokuseiDownload(Summer2025AnimeDownload, NewsTemplate):
         self.download_news()
 
     def download_episode_preview(self):
-        self.has_website_updated(self.PAGE_PREFIX, 'index')
+        template = self.PAGE_PREFIX + 'assets/images/story/ep%s/img%s.jpg'
+        try:
+            stop = False
+            for i in range(self.FINAL_EPISODE):
+                episode = str(i + 1).zfill(2)
+                if self.is_image_exists(episode + '_' + str(self.IMAGES_PER_EPISODE)):
+                    continue
+                for j in range(self.IMAGES_PER_EPISODE):
+                    image_url = template % (str(i + 1), str(j + 1))
+                    image_name = episode + '_' + str(j + 1)
+                    if self.download_image(image_url, self.base_folder + '/' + image_name, to_jpg=True) == -1:
+                        stop = True
+                        break
+                if stop:
+                    break
+        except Exception as e:
+            self.print_exception(e)
 
     def download_news(self):
         self.download_template_news(page_prefix=self.PAGE_PREFIX, article_select='.news-list .item',
