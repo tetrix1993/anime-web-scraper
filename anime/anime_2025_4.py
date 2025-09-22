fb4b3da4f38cda266cb0660e52e910b93ebe2135
tetrix1993 <tetrix1993@hotmail.com>
2025-09-22 16:13:47 +0800

    Added TowanoYuugure Episode Preview

--- anime/anime_2025_4.py
+++ anime/anime_2025_4.py
@@ -680,7 +680,37 @@ class TowanoYuugureDownload(Fall2025AnimeDownload, NewsTemplate):
         self.download_news()
 
     def download_episode_preview(self):
-        self.has_website_updated(self.PAGE_PREFIX)
+        try:
+            soup = self.get_soup(self.PAGE_PREFIX)
+            styles = soup.select('style[type="text/css"]')
+            self.image_list = []
+            for style in styles:
+                if '#js-story-slider[data-ep="0"]' not in style.text:
+                    continue
+                s = style.text.split('}')
+                for s2 in s:
+                    if '#js-story-slider[data-ep="' in s2:
+                        s3 = s2.split('#js-story-slider[data-ep="')
+                        for t in s3:
+                            if 'li:nth-child(' not in t:
+                                continue
+                            try:
+                                episode = str(int(t.split('"')[0])).zfill(2)
+                                img_num = str(int(t.split('li:nth-child(')[1].split(')')[0].strip()))
+                                image_name = episode + '_' + img_num
+                            except:
+                                continue
+                            if self.is_image_exists(image_name):
+                                continue
+                            if 'url(' in t and ');' in t:
+                                start_idx = t.find('url(') + 4
+                                end_idx = t.find(');')
+                                if start_idx < end_idx:
+                                    image_url = t[start_idx: end_idx]
+                                    self.add_to_image_list(image_name, image_url)
+            self.download_image_list(self.base_folder)
+        except Exception as e:
+            self.print_exception(e)
 
     def download_news(self):
         self.download_template_news(page_prefix=self.PAGE_PREFIX, article_select='.news a', date_select='time',
