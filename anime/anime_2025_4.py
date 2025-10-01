a37b7d688e8d65408fa6aa46015c5d6d1e0130b6
tetrix1993 <tetrix1993@hotmail.com>
2025-10-01 11:19:00 +0800

    Added MugenGacha Episode Preview

--- anime/anime_2025_4.py
+++ anime/anime_2025_4.py
@@ -513,7 +513,38 @@ class MugenGachaDownload(Fall2025AnimeDownload, NewsTemplate):
         self.download_news()
 
     def download_episode_preview(self):
-        self.has_website_updated(self.PAGE_PREFIX)
+        try:
+            soup = self.get_soup(self.PAGE_PREFIX + 'story/')
+            stories = soup.select('.story_nav_slide a[href]')
+            for story in stories:
+                try:
+                    episode = ''
+                    ep_num = story.text
+                    is_num = False
+                    for a in ep_num:
+                        if a.isnumeric():
+                            episode += a
+                            is_num = True
+                        elif is_num:
+                            break
+                    if len(episode) == 0:
+                        continue
+                    episode = str(int(episode)).zfill(2)
+                except:
+                    continue
+                if self.is_image_exists(episode + '_1'):
+                    continue
+                ep_soup = self.get_soup(story['href'])
+                if ep_soup is None:
+                    continue
+                images = ep_soup.select('.story_scene_slide img[src]')
+                for i in range(len(images)):
+                    image_url = images[i]['src']
+                    image_name = episode + '_' + str(i + 1).zfill(2)
+                    self.add_to_image_list(image_name, image_url, to_jpg=True)
+                self.download_image_list(self.base_folder)
+        except Exception as e:
+            self.print_exception(e)
 
     def download_news(self):
         self.download_template_news(page_prefix=self.PAGE_PREFIX, article_select='.news_item', date_select='time',
