c77aae748f315da9f17c4c435f0b0e120e620451
tetrix1993 <tetrix1993@hotmail.com>
2025-10-01 20:03:44 +0800

    Added FumetsuNoAnatae3 Episode Preview

--- anime/anime_2025_4.py
+++ anime/anime_2025_4.py
@@ -406,6 +406,7 @@ class FumetsuNoAnatae3Download(Fall2025AnimeDownload, NewsTemplate):
 
     PAGE_PREFIX = website
     FINAL_EPISODE = 22
+    IMAGES_PER_EPISODE = 6
 
     def __init__(self):
         super().__init__()
@@ -415,7 +416,23 @@ class FumetsuNoAnatae3Download(Fall2025AnimeDownload, NewsTemplate):
         self.download_news()
 
     def download_episode_preview(self):
-        self.has_website_updated(self.PAGE_PREFIX)
+        template = self.PAGE_PREFIX + 'wp-content/themes/fumetsu/assets/img/episode/%s/img%s.jpg'
+        try:
+            stop = False
+            for i in range(self.FINAL_EPISODE):
+                episode = str(i + 1).zfill(2)
+                if self.is_image_exists(episode + '_' + str(self.IMAGES_PER_EPISODE)):
+                    continue
+                for j in range(self.IMAGES_PER_EPISODE):
+                    image_url = template % (str(i + 1), str(j + 1))
+                    image_name = episode + '_' + str(j + 1)
+                    if self.download_image(image_url, self.base_folder + '/' + image_name, to_jpg=True) == -1:
+                        stop = True
+                        break
+                if stop:
+                    break
+        except Exception as e:
+            self.print_exception(e)
 
     def download_news(self):
         self.download_template_news(page_prefix=self.PAGE_PREFIX, article_select='.c-post__list-item',
