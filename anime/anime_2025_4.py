8af75c50ee43e58347f548ba80c77cb8d40d689d
tetrix1993 <tetrix1993@hotmail.com>
2025-10-15 17:23:50 +0800

    Fixed FumetsuNoAnatae3 Episode Preview

--- anime/anime_2025_4.py
+++ anime/anime_2025_4.py
@@ -474,7 +474,7 @@ class FumetsuNoAnatae3Download(Fall2025AnimeDownload, NewsTemplate):
             stop = False
             for i in range(self.FINAL_EPISODE):
                 episode = str(i + 1).zfill(2)
-                if self.is_image_exists(episode + '_' + str(self.IMAGES_PER_EPISODE)):
+                if self.is_image_exists(episode + '_1'):
                     continue
                 for j in range(self.IMAGES_PER_EPISODE):
                     image_url = template % (str(i + 1), str(j + 1))
