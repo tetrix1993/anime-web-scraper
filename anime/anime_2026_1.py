49711027a9ceda2d113e90d24a382bc9525b5e74
tetrix1993 <tetrix1993@hotmail.com>
2025-12-29 23:11:53 +0800

    Added Yushakei Episode Preview

--- anime/anime_2026_1.py
+++ anime/anime_2026_1.py
@@ -643,6 +643,8 @@ class YushakeiDownload(Winter2026AnimeDownload):
     folder_name = 'yushakei'
 
     PAGE_PREFIX = website
+    FINAL_EPISODE = 12
+    IMAGES_PER_EPISODE = 8
 
     def __init__(self):
         super().__init__()
@@ -652,7 +654,23 @@ class YushakeiDownload(Winter2026AnimeDownload):
         self.download_news()
 
     def download_episode_preview(self):
-        pass
+        template = self.PAGE_PREFIX + 'episodes/img/%s/%s.jpg'
+        try:
+            stop = False
+            for i in range(self.FINAL_EPISODE):
+                episode = str(i + 1).zfill(2)
+                if self.is_image_exists(episode + '_' + str(self.IMAGES_PER_EPISODE)):
+                    continue
+                for j in range(self.IMAGES_PER_EPISODE):
+                    image_url = template % (str(i + 1), str(j + 1))
+                    image_name = episode + '_' + str(j + 1)
+                    if self.download_image(image_url, self.base_folder + '/' + image_name, to_jpg=True) == -1:
+                        stop = True
+                        break
+                if stop:
+                    break
+        except Exception as e:
+            self.print_exception(e)
 
     def download_news(self):
         try:
