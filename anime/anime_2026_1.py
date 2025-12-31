68ca040bda1e46f2bbb37c769424b1d323ddc4a6
tetrix1993 <tetrix1993@hotmail.com>
2025-12-31 20:27:17 +0800

    Added KiyouBimbou Episode Preview

--- anime/anime_2026_1.py
+++ anime/anime_2026_1.py
@@ -624,7 +624,32 @@ class KiyouBimbouDownload(Winter2026AnimeDownload, NewsTemplate):
         self.download_news()
 
     def download_episode_preview(self):
-        pass
+        try:
+            soup = self.get_soup(self.PAGE_PREFIX)
+            stories = soup.select('.story-container')
+            for story in stories:
+                try:
+                    episode = ''
+                    ep_num = story.select('.story-detail-ttl')[0].text.split('話')[0]
+                    for a in reversed(ep_num):
+                        if a.isnumeric():
+                            episode = a + episode
+                    if len(episode) == 0:
+                        continue
+                    episode = str(int(episode)).zfill(2)
+                except:
+                    continue
+                if self.is_image_exists(episode + '_1'):
+                    continue
+                images = story.select('.story-thumb-item img[src]')
+                self.image_list = []
+                for i in range(len(images)):
+                    image_url = images[i]['src']
+                    image_name = episode + '_' + str(i + 1)
+                    self.add_to_image_list(image_name, image_url, to_jpg=True)
+                self.download_image_list(self.base_folder)
+        except Exception as e:
+            self.print_exception(e)
 
     def download_news(self):
         self.download_template_news(page_prefix=self.PAGE_PREFIX, article_select='.news-box',
