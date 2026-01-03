d1d0fb90b2885fb533414fabd445ebe4c2609051
tetrix1993 <tetrix1993@hotmail.com>
2026-01-03 08:14:32 +0800

    Added Osalove Episode Preview

--- anime/anime_2026_1.py
+++ anime/anime_2026_1.py
@@ -509,7 +509,35 @@ class OsaloveDownload(Winter2026AnimeDownload, NewsTemplate):
         self.download_news()
 
     def download_episode_preview(self):
-        pass
+        try:
+            soup = self.get_soup(self.PAGE_PREFIX + 'episode/')
+            stories = soup.select('.newsCategory__anc[href]')
+            for story in stories:
+                try:
+                    episode = ''
+                    ep_num = story.text
+                    for a in reversed(ep_num):
+                        if a.isnumeric():
+                            episode = a + episode
+                    if len(episode) == 0:
+                        continue
+                    episode = str(int(episode)).zfill(2)
+                except:
+                    continue
+                if self.is_image_exists(episode + '_1'):
+                    continue
+                ep_soup = self.get_soup(story['href'])
+                if ep_soup is None:
+                    continue
+                images = ep_soup.select('img.mvSlider__main__item__img[src]')
+                self.image_list = []
+                for i in range(len(images)):
+                    image_url = images[i]['src']
+                    image_name = episode + '_' + str(i + 1)
+                    self.add_to_image_list(image_name, image_url, to_jpg=True)
+                self.download_image_list(self.base_folder)
+        except Exception as e:
+            self.print_exception(e)
 
     def download_news(self):
         self.download_template_news(page_prefix=self.PAGE_PREFIX, article_select='.newsCol',
