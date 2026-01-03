09b4ae95bc31f97ce58ffa320bb87b0f9a7088b4
tetrix1993 <tetrix1993@hotmail.com>
2026-01-04 00:47:35 +0800

    Added Osalove Guess

--- anime/anime_2026_1.py
+++ anime/anime_2026_1.py
@@ -500,6 +500,8 @@ class OsaloveDownload(Winter2026AnimeDownload, NewsTemplate):
     folder_name = 'osalove'
 
     PAGE_PREFIX = website
+    FINAL_EPISODE = 12
+    IMAGES_PER_EPISODE = 6
 
     def __init__(self):
         super().__init__()
@@ -543,6 +545,45 @@ class OsaloveDownload(Winter2026AnimeDownload, NewsTemplate):
         self.download_template_news(page_prefix=self.PAGE_PREFIX, article_select='.newsCol',
                                     date_select='.newsCol__date', title_select='.newsCol__title', id_select=None,
                                     next_page_select='#load-more')
+
+    def download_episode_preview_guess(self, print_url=False, print_invalid=False):
+        if self.is_image_exists(str(self.FINAL_EPISODE).zfill(2) + '_1'):
+            return
+        folder = self.create_custom_directory('guess')
+        template = self.PAGE_PREFIX + 'websys/wp-content/uploads/%s/%s/%s_%s.'
+        extensions = ['jpg']
+        current_date = datetime.now() + timedelta(hours=1)
+        year = current_date.strftime('%Y')
+        month = current_date.strftime('%m')
+        is_successful = False
+        for i in range(self.FINAL_EPISODE):
+            episode = str(i + 1).zfill(2)
+            if self.is_image_exists(episode + '_1') or self.is_image_exists(episode + '_1', folder):
+                continue
+            is_success = False
+            for ext in extensions:
+                for j in range(self.IMAGES_PER_EPISODE):
+                    image_name = episode + '_' + str(j + 1)
+                    f = '01★メインカット★' if j == 0 else str(j + 1).zfill(2)
+                    image_url = (template % (year, month, episode, f) + ext)
+                    if print_url:
+                        print(image_url)
+                    if self.is_valid_url(image_url, is_image=True):
+                        print('VALID - ' + image_url)
+                        self.download_image(image_url, folder + '/' + image_name, to_jpg=True)
+                        is_successful = True
+                        is_success = True
+                    elif print_invalid:
+                        print('INVALID - ' + image_url)
+                    # if not is_success:
+                    #     break
+                if is_success:
+                    break
+            if not is_success:
+                break
+        if is_successful:
+            print(self.__class__.__name__ + ' - Guessed correctly!')
+        return is_successful
 
 
 # Shibou Yuugi de Meshi wo Kuu.
