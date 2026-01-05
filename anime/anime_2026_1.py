5596a5f28688863b2f5ebd548592f0487a7370ce
tetrix1993 <tetrix1993@hotmail.com>
2026-01-05 20:13:54 +0800

    Added Maomusu Episode Preview

--- anime/anime_2026_1.py
+++ anime/anime_2026_1.py
@@ -417,6 +417,8 @@ class MaomusuDownload(Winter2026AnimeDownload, NewsTemplate):
     folder_name = 'maomusu'
 
     PAGE_PREFIX = website
+    FINAL_EPISODE = 12
+    IMAGES_PER_EPISODE = 5
 
     def __init__(self):
         super().__init__()
@@ -426,7 +428,23 @@ class MaomusuDownload(Winter2026AnimeDownload, NewsTemplate):
         self.download_news()
 
     def download_episode_preview(self):
-        pass
+        template = self.PAGE_PREFIX + 'assets/img/story/ep%s/img%s.jpg'
+        try:
+            stop = False
+            for i in range(self.FINAL_EPISODE):
+                episode = str(i + 1).zfill(2)
+                if self.is_image_exists(episode + '_' + str(self.IMAGES_PER_EPISODE)):
+                    continue
+                for j in range(self.IMAGES_PER_EPISODE):
+                    image_url = template % (str(i + 1), str(j + 1).zfill(2))
+                    image_name = episode + '_' + str(j + 1)
+                    if self.download_image(image_url, self.base_folder + '/' + image_name, to_jpg=True) == -1:
+                        stop = True
+                        break
+                if stop:
+                    break
+        except Exception as e:
+            self.print_exception(e)
 
     def download_news(self):
         self.download_template_news(page_prefix=self.PAGE_PREFIX, article_select='.bl_posts_item',
