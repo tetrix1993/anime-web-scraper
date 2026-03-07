bd6dcac92e173f8675fad19a5d51252d8d43fd1a
tetrix1993 <tetrix1993@hotmail.com>
2026-03-07 19:37:03 +0800

    Added Kuranika

--- anime/anime_2026_2.py
+++ anime/anime_2026_2.py
@@ -41,3 +41,30 @@ class AishiteruGameDownload(Spring2026AnimeDownload, NewsTemplate):
                                     title_select='.entry-title span', date_select='.entry-date span',
                                     id_select=None, id_has_id=True, news_prefix='news.html',
                                     a_tag_prefix=self.PAGE_PREFIX + 'news.html#')
+
+
+# Class de 2-banme ni Kawaii Onnanoko to Tomodachi ni Natta
+class KuranikaDownload(Spring2026AnimeDownload, NewsTemplate):
+    title = 'Class de 2-banme ni Kawaii Onnanoko to Tomodachi ni Natta'
+    keywords = [title, 'I Made Friends with the Second Prettiest Girl in My Class', 'kuranika']
+    website = 'https://kuranika.asmik-ace.co.jp/'
+    twitter = 'kuranika'
+    hashtags = 'クラにか'
+    folder_name = 'kuranika'
+
+    PAGE_PREFIX = website
+
+    def __init__(self):
+        super().__init__()
+
+    def run(self):
+        self.download_episode_preview()
+        self.download_news()
+
+    def download_episode_preview(self):
+        pass
+
+    def download_news(self):
+        self.download_template_news(page_prefix=self.PAGE_PREFIX, article_select='.bl_posts_item', date_select='time',
+                                    title_select='.bl_posts_txt', id_select='a', next_page_select='a.next.page-numbers',
+                                    paging_type=3, paging_suffix='?page=%s', date_separator='/')
