97b711ee7a00f406c0819b2b9db4f390dc182bd7
tetrix1993 <tetrix1993@hotmail.com>
2026-03-29 19:55:15 +0800

    Added Matakoro Episode Preview

--- anime/anime_2026_2.py
+++ anime/anime_2026_2.py
@@ -469,6 +469,8 @@ class MatakoroDownload(Spring2026AnimeDownload, NewsTemplate):
     folder_name = 'matakoro'
 
     PAGE_PREFIX = website
+    FINAL_EPISODE = 12
+    IMAGES_PER_EPISODE = 6
 
     def __init__(self):
         super().__init__()
@@ -478,12 +480,28 @@ class MatakoroDownload(Spring2026AnimeDownload, NewsTemplate):
         self.download_news()
 
     def download_episode_preview(self):
-        pass
+        template = self.PAGE_PREFIX + 'story/img/v%s_%s.jpg'
+        try:
+            stop = False
+            for i in range(self.FINAL_EPISODE):
+                episode = str(i + 1).zfill(2)
+                if self.is_image_exists(episode + '_' + str(self.IMAGES_PER_EPISODE)):
+                    continue
+                for j in range(self.IMAGES_PER_EPISODE):
+                    image_url = template % (str(i + 1), str(j + 1))
+                    image_name = episode + '_' + str(j + 1)
+                    if self.download_image(image_url, self.base_folder + '/' + image_name, to_jpg=True) == -1:
+                        stop = True
+                        break
+                if stop:
+                    break
+        except Exception as e:
+            self.print_exception(e)
 
     def download_news(self):
         self.download_template_news(page_prefix=self.PAGE_PREFIX, article_select='#news-list>li',
                                     date_select='dt', title_select='dd', id_select='a',
-                                    a_tag_prefix=self.PAGE_PREFIX + 'news/')
+                                    a_tag_prefix='https://www.tbs.co.jp')
 
 
 # Nigashita Sakana wa Ookikatta ga Tsuriageta Sakana ga Ookisugita Ken
