f66f9980215a24e806e59bbdf784b6e3a0b70c7d
tetrix1993 <tetrix1993@hotmail.com>
2026-04-03 11:24:27 +0800

    Added Nekomajo

--- anime/anime_2026_2.py
+++ anime/anime_2026_2.py
@@ -467,7 +467,24 @@ class NekomajoDownload(Spring2026AnimeDownload, NewsTemplate):
         self.download_news()
 
     def download_episode_preview(self):
-        pass
+        try:
+            soup = self.get_soup(self.PAGE_PREFIX)
+            eps = soup.select('.episode')
+            for ep in eps:
+                try:
+                    episode = str(int(ep.select('.episode__number .number')[0].text)).zfill(2)
+                except:
+                    continue
+                if self.is_image_exists(episode + '_1'):
+                    continue
+                images = ep.select('.slide__image img.episode_image[src]')
+                for i in range(len(images)):
+                    image_url = self.clear_resize_in_url(images[i]['src'])
+                    image_name = episode + '_' + str(i + 1)
+                    self.add_to_image_list(image_name, image_url, to_jpg=True)
+                self.download_image_list(self.base_folder)
+        except Exception as e:
+            self.print_exception(e)
 
     def download_news(self):
         self.download_template_news(page_prefix=self.PAGE_PREFIX, article_select='.entry-title',
