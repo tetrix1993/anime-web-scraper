b89af5091d041a43d9ca9912a068becfee5e273f
tetrix1993 <tetrix1993@hotmail.com>
2026-04-06 21:03:45 +0800

    Added LiarGame Episode Preview

--- anime/anime_2026_2.py
+++ anime/anime_2026_2.py
@@ -571,7 +571,37 @@ class LiarGameDownload(Spring2026AnimeDownload, NewsTemplate):
         self.download_news()
 
     def download_episode_preview(self):
-        pass
+        try:
+            soup = self.get_soup(self.PAGE_PREFIX + 'story/')
+            a_tags = soup.select('a[href]')
+            for a_tag in a_tags:
+                episode = ''
+                try:
+                    ep_num = a_tag.text
+                    for a in reversed(ep_num):
+                        if a.isnumeric():
+                            episode = a + episode
+                    if len(episode) == 0:
+                        continue
+                    episode = str(int(episode)).zfill(2)
+                except:
+                    continue
+                if self.is_image_exists(episode + '_1'):
+                    continue
+                if 'is-Current' in a_tag['class']:
+                    ep_soup = soup
+                else:
+                    ep_soup = self.get_soup(a_tag['href'])
+                if ep_soup is None:
+                    continue
+                images = ep_soup.select('.p-story__sceneImage-item img[src]')
+                for i in range(len(images)):
+                    image_url = images[i]['src']
+                    image_name = episode + '_' + str(i + 1)
+                    self.add_to_image_list(image_name, image_url, to_jpg=True)
+                self.download_image_list(self.base_folder)
+        except Exception as e:
+            self.print_exception(e)
 
     def download_news(self):
         self.download_template_news(page_prefix=self.PAGE_PREFIX, article_select='.archive__item',
