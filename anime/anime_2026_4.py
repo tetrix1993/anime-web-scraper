989cd544b0ba37f513fb8ca5a03628ad7a734d3d
tetrix1993 <tetrix1993@hotmail.com>
2026-09-20 16:10:25 +0800

    Added MaryokuRevo Episode Preview

--- anime/anime_2026_4.py
+++ anime/anime_2026_4.py
@@ -56,7 +56,33 @@ class MaryokuRevoDownload(Fall2026AnimeDownload, NewsTemplate):
         self.download_news()
 
     def download_episode_preview(self):
-        pass
+        try:
+            soup = self.get_soup(self.PAGE_PREFIX + 'story/')
+            stories = soup.select('.story-list a[href]')
+            for story in stories:
+                try:
+                    episode = ''
+                    ep_num = story.select('.story-ep')[0].text
+                    for a in ep_num:
+                        if a.isnumeric():
+                            episode += a
+                    if len(episode) == 0:
+                        continue
+                    episode = str(int(episode)).zfill(2)
+                except:
+                    continue
+                ep_soup = self.get_soup(story['href'])
+                if ep_soup is None:
+                    continue
+                self.image_list = []
+                images = ep_soup.select('#story-main .splide__slide img[src]')
+                for i in range(len(images)):
+                    image_url = images[i]['src']
+                    image_name = episode + '_' + str(i + 1)
+                    self.add_to_image_list(image_name, image_url, to_jpg=True)
+                self.download_image_list(self.base_folder)
+        except Exception as e:
+            self.print_exception(e)
 
     def download_news(self):
         self.download_template_news(page_prefix=self.PAGE_PREFIX, article_select='a', date_select='.news-date',
