e681411026f4ccd5b4bd7ef10e8489c0d8030d56
tetrix1993 <tetrix1993@hotmail.com>
2026-09-24 17:26:43 +0800

    Added Majieku Episode Preview

--- anime/anime_2026_4.py
+++ anime/anime_2026_4.py
@@ -196,7 +196,39 @@ class MajiekuDownload(Fall2026AnimeDownload, NewsTemplate):
         self.download_news()
 
     def download_episode_preview(self):
-        pass
+        episode_url = self.PAGE_PREFIX + 'episodes/'
+        try:
+            soup = self.get_soup(episode_url, decode=True)
+            stories = soup.select('.story__navList a[href]')
+            for story in stories:
+                try:
+                    episode = ''
+                    ep_num = story.text
+                    for a in ep_num:
+                        if a.isnumeric():
+                            episode += a
+                    if len(episode) == 0:
+                        continue
+                    episode = str(int(episode)).zfill(2)
+                except:
+                    continue
+                if self.is_image_exists(episode + '_1'):
+                    continue
+                if '--is-current' in story['class']:
+                    ep_soup = soup
+                else:
+                    ep_soup = self.get_soup(episode_url + story['href'])
+                if ep_soup is None:
+                    continue
+                self.image_list = []
+                images = ep_soup.select('.story__imageList img[src]')
+                for i in range(len(images)):
+                    image_url = episode_url + images[i]['src']
+                    image_name = episode + '_' + str(i + 1)
+                    self.add_to_image_list(image_name, image_url, to_jpg=True)
+                self.download_image_list(self.base_folder)
+        except Exception as e:
+            self.print_exception(e)
 
     def download_news(self):
         news_url = self.PAGE_PREFIX + 'news/'
